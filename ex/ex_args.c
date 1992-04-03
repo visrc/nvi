a95cc71f396b5f82c8f4679bf8836f3cea83f056
bostic <bostic>
1992-04-03 08:25:53 +0000

    pass #2 -- have an argv to start with, lots of rewrite

--- ex/ex_args.c
+++ ex/ex_args.c
@@ -6,111 +6,92 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_args.c,v 5.2 1992/01/17 09:48:37 bostic Exp $ (Berkeley) $Date: 1992/01/17 09:48:37 $";
+static char sccsid[] = "$Id: ex_args.c,v 5.3 1992/04/03 08:25:53 bostic Exp $ (Berkeley) $Date: 1992/04/03 08:25:53 $";
 #endif /* not lint */
 
+#include <sys/types.h>
 #include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include "config.h"
+
 #include "vi.h"
+#include "excmd.h"
 #include "extern.h"
 
 static char	**s_flist;		/* start of file list */
 static int	total, current;
 
-static void	set_file_buf __P((char *));
-
 /*
- * next_file -- (:next [files])
+ * cmd_next -- (:next [files])
  *	Edit the next file.
  */
 void
-next_file(frommark, tomark, cmd, bang, extra)
-	MARK frommark, tomark;
-	CMD cmd;
-	int bang;
-	char *extra;
+ex_next(cmdp)
+	CMDARG *cmdp;
 {
 	/* It's permissible to specify a list of files with :next. */
-	if (extra)
-		set_file_buf(extra);
-
-	/* Special case for starting a list. */
-	if (current == 0) {
-		tmpstart(s_flist[0]);
-		current = 1;
-		return;
-	}
+	if (cmdp->argc)
+		file_set(cmdp->argc, cmdp->argv, 1);
 
 	if (current < total)
-		if (tmpabort(bang)) {
+		if (tmpabort(cmdp->flags & E_FORCE)) {
 			tmpstart(s_flist[current]);
 			++current;
 		} else
-			msg("use :next! to discard changes, or w to save them");
+			msg("Use next! to discard changes, or w to save them.");
 	else
-		msg("No more files to edit");
+		msg("No more files to edit.");
 }
 
 /*
- * prev_file -- (:prev)
+ * file_prev -- (:prev)
  *	Edit the previous file.
  */
 void
-prev_file(frommark, tomark, cmd, bang, extra)
-	MARK frommark, tomark;
-	CMD cmd;
-	int bang;
-	char *extra;
+ex_prev(cmdp)
+	CMDARG *cmdp;
 {
 	if (current > 1)
-		if (tmpabort(bang)) {
+		if (tmpabort(cmdp->flags & E_FORCE)) {
 			--current;
 			tmpstart(s_flist[current - 1]);
 		} else
-			msg("use :prev! to discard changes, or w to save them");
+			msg("Use prev! to discard changes, or w to save them.");
 	else
-		msg("No previous files to edit");
+		msg("No previous files to edit.");
 }
 
 /*
- * rew_file -- (:rew)
+ * file_rew -- (:rew)
  *	Edit the first file.
  */
 void
-rew_file(frommark, tomark, cmd, bang, extra)
-	MARK frommark, tomark;
-	CMD cmd;
-	int bang;
-	char *extra;
+ex_rew(cmdp)
+	CMDARG *cmdp;
 {
 	if (current > 1)
-		if (tmpabort(bang)) {
+		if (tmpabort(cmdp->flags & E_FORCE)) {
 			tmpstart(s_flist[0]);
 			current = 1;
 		} else
-			msg("use :rew! to discard changes, or w to save them");
+			msg("Use rew! to discard changes, or w to save them.");
 	else
-		msg("No previous files to edit");
+		msg("No previous files to edit.");
 }
 
 /*
- * args_file -- (:args [files])
- *	Display the list of files or set the list of files.
+ * file_args -- (:args)
+ *	Display the list of files.
  */
 void
-args_file(frommark, tomark, cmd, bang, extra)
-	MARK frommark, tomark;
-	CMD cmd;
-	int bang;
-	char *extra;
+ex_args(cmdp)
+	CMDARG *cmdp;
 {
 	register char **p, *sep;
 	int cnt, col, len, newline;
 
 	if (current == 0) {
-		msg("No file names");
+		msg("No file names.");
 		return;
 	}
 
@@ -138,81 +119,49 @@ args_file(frommark, tomark, cmd, bang, extra)
 }
 
 /*
- * set_file --
+ * file_set --
  *	Set the file list from an argc/argv.
  */
 void
-set_file(argc, argv)
-	int argc;
+file_set(argc, argv, copy)
+	int argc, copy;
 	char *argv[];
 {
-	s_flist = argv;
-	current = 0;
-	total = argc;
-}
-	
-/*
- * set_file_buf --
- *	Set the file list from a white-space separated buffer.
- *
- * XXX
- * The command parsing routines should have already broken out an
- * argv before we get here.
- */
-static void
-set_file_buf(buf)
-	char *buf;
-{
-	static int allocated;
+	static char **copyav;
 	int cnt;
-	char **ap, **argv, *bp, *p, *start;
-	
-	/* Figure out how many file names and allocate space for them. */
-	if ((bp = start = strdup(buf)) == NULL)
-		goto mem3;
-	for (cnt = 1; (p = strsep(&bp, " \t")) != NULL;)
-		if (*p != '\0')
-			++cnt;
-	free(start);
+	char **avp;
 
-	if ((argv = malloc((size_t)(cnt * sizeof(char *)))) == NULL)
-		goto mem3;
-	if ((bp = start = strdup(buf)) == NULL) 
-		goto mem2;
-
-	/* Put the names into the array, and NULL terminate it. */
-	for (ap = argv; (*ap = strsep(&bp, " \t")) != NULL;)
-		if (**ap != '\0') {
-			if ((*ap = strdup(*ap)) == NULL)
-				goto mem1;
-			++ap;
+	/* Copy the array and strings if necessary. */
+	if (copy) {
+		if (copyav) {
+			for (avp = copyav; *avp; ++avp)
+				free(*avp);
+			free(copyav);
 		}
-	*ap = NULL;
-
-	if (allocated) {
-		for (ap = s_flist; *ap; ++ap)
-			free(*ap);
-		free(s_flist);
+		if ((copyav = malloc((argc + 1) * sizeof(char *))) == NULL) {
+			msg("Error: %s.", strerror(errno));
+			return;
+		}
+		for (cnt = 0; cnt < argc; ++cnt)
+			if ((copyav[cnt] = strdup(argv[cnt])) == NULL) {
+				msg("Error: %s.", strerror(errno));
+				break;
+			}
+		copyav[cnt] = NULL;
+		s_flist = copyav;
 	} else
-		allocated = 1;
+		s_flist = argv;
 
-	set_file(cnt - 1, argv);
-	return;
-
-mem1:	while (--ap >= argv)
-		free(*ap);
-	free(start);
-mem2:	free(argv);
-mem3:	msg("args failed: %s", strerror(errno));
-	return;
+	total = argc;
+	current = 0;
 }
 
 /*
- * cnt_file --
+ * file_cnt --
  *	Return a count of the files left to edit.
  */
 int
-cnt_file()
+file_cnt()
 {
 	return (total - current);
 }
