e641919a0c817bf56e425a2e6e70686a1c29c5bc
bostic <bostic>
1992-04-03 08:23:27 +0000

    delete config.h

--- common/key.c
+++ common/key.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: key.c,v 5.4 1992/02/20 14:15:10 bostic Exp $ (Berkeley) $Date: 1992/02/20 14:15:10 $";
+static char sccsid[] = "$Id: key.c,v 5.5 1992/04/03 08:23:27 bostic Exp $ (Berkeley) $Date: 1992/04/03 08:23:27 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -14,7 +14,6 @@ static char sccsid[] = "$Id: key.c,v 5.4 1992/02/20 14:15:10 bostic Exp $ (Berke
 #include <unistd.h>
 #include <stdio.h>
 
-#include "config.h"
 #include "options.h"
 #include "vi.h"
 #include "extern.h"
