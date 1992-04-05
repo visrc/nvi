026a6a22acba79461dd2b5ba86e25cba3b2eca93
bostic <bostic>
1992-04-05 15:47:44 +0000

    generalize the map functions

--- common/seq.h
+++ common/seq.h
@@ -4,52 +4,66 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: seq.h,v 5.2 1992/04/05 09:46:02 bostic Exp $ (Berkeley) $Date: 1992/04/05 09:46:02 $
+ *	$Id: seq.h,v 5.3 1992/04/05 15:47:44 bostic Exp $ (Berkeley) $Date: 1992/04/05 15:47:44 $
  */
 
 /*
- * The map structure is an UCHAR_MAX size array of MAP pointers which
- * are NULL or valid depending if the key starts any mapping sequences.
- * If the pointer is valid, it references a doubly linked list of MAP
- * pointers, threaded through the next and prev pointers.  This is
- * intended to support fast lookup on non-mapped characters and much
- * too much flexibility for mapped strings in general.  In order to
- * keep the overhead of the map array fairly small, only a single
- * pointer is maintained, so the first element of the linked list has
- * a NULL prev pointer and the last element of the list has a NULL
- * next pointer.
+ * Map and abbreviation structures.
  *
- * In addition, each MAP structure is on another doubly linked list
- * of MAP pointers, threaded through the lnext and lprev pointers.
- * This is a linked list of all of the mapped sequences, headed by
- * the mhead MAPLIST structure.  This structure is used by the routines
- * that display all of the map entries on the screen or write them to
- * a file.
+ * The map structure is an UCHAR_MAX size array of SEQ pointers which are
+ * NULL or valid depending if the offset key begins any map or abbreviation
+ * sequences.  If the pointer is valid, it references a doubly linked list
+ * of SEQ pointers, threaded through the next and prev pointers.  This is
+ * based on the belief that most normal characters won't start sequences so
+ * lookup will be fast on non-mapped characters.  Only a single pointer is
+ * maintained to keep the overhead of the map array fairly small, so the first
+ * element of the linked list has a NULL prev pointer and the last element
+ * of the list has a NULL next pointer.  The structures in this list are
+ * ordered by length, shortest to longest.  This is so that short matches 
+ * will happen before long matches when the list is searched.
+ *
+ * In addition, each SEQ structure is on another doubly linked list of SEQ
+ * pointers, threaded through the lnext and lprev pointers.  This is a list
+ * of all of the sequences, headed by the mhead SEQLIST structure.  This list
+ * is used by the routines that display all of the sequences to the screen
+ * or write them to a file.
  */
-enum whenmapped { COMMAND, INPUT };	/* When mapping in effect. */
+					/* Sequence type. */
+enum seqtype { ABBREV, COMMAND, INPUT };
 
-typedef struct _map {
-	struct _map *next, *prev;	/* Linked list from map array. */
-	struct _map *lnext, *lprev;	/* Linked list of all entries. */
-	char *name;			/* Name of the map, if any. */
+typedef struct _seq {
+	struct _seq *next, *prev;	/* Linked list of ch sequences. */
+	struct _seq *lnext, *lprev;	/* Linked list of all sequences. */
+	char *name;			/* Name of the sequence, if any. */
 	char *input;			/* Input key sequence. */
 	char *output;			/* Output key sequence. */
 	int ilen;			/* Input key sequence length. */
-	enum whenmapped when;		/* When mapping in effect. */
+	enum seqtype stype;		/* Sequence type. */
 
-#define	M_USERDEF	0x01		/* If mapping user defined. */
+#define	S_USERDEF	0x01		/* If sequence user defined. */
 	u_char flags;
-} MAP;
+} SEQ;
+extern SEQ *seq[UCHAR_MAX];		/* Linked list of ch sequences. */
 
-typedef struct _mhead {
-	struct _map *lnext, *lprev;	/* Linked list of all entries. */
-} MAPLIST;
+typedef struct {
+	SEQ *lnext, *lprev;
+} SEQLIST;
+extern SEQLIST seqhead;			/* Linked list of all sequences. */
 
-#define	ismapped(ch)	(map[(ch)])	/* If ch starts any map entries. */
+					/* If need to check abbreviations. */
+#define	chkabbr(ch)	(have_abbr && !inword(ch))
+#define	seqstart(ch)	(seq[(ch)])	/* If ch starts any sequences. */
 
-extern MAPLIST mhead;			/* All entries map structure. */
-extern MAP *map[UCHAR_MAX];		/* Character map structure. */
+extern int have_abbr;			/* If any abbreviations. */
+MARK	 abbr_expand __P((MARK, MARK *));
+int	 abbr_save __P((FILE *));
 
 int	 map_init __P((void));
 int	 map_save __P((FILE *));
-MAP	*map_find __P((u_char *, enum whenmapped, int *));
+
+int	 seq_delete __P((char *, enum seqtype));
+int	 seq_dump __P((enum seqtype));
+SEQ	*seq_find __P((char *, size_t, enum seqtype, int *));
+void	 seq_init __P((void));
+int	 seq_save __P((FILE *, char *, enum seqtype));
+int	 seq_set __P((char *, char *, char *, enum seqtype, int));
--- ex/ex_map.c
+++ ex/ex_map.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_map.c,v 5.4 1992/04/05 11:01:43 bostic Exp $ (Berkeley) $Date: 1992/04/05 11:01:43 $";
+static char sccsid[] = "$Id: ex_map.c,v 5.5 1992/04/05 15:48:04 bostic Exp $ (Berkeley) $Date: 1992/04/05 15:48:04 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -17,15 +17,9 @@ static char sccsid[] = "$Id: ex_map.c,v 5.4 1992/04/05 11:01:43 bostic Exp $ (Be
 #include <string.h>
 
 #include "vi.h"
-#include "map.h"
+#include "seq.h"
 #include "excmd.h"
 #include "extern.h"
-
-static int map_dump __P((enum whenmapped));
-static int map_set __P((char *, char *, char *, enum whenmapped, int));
-
-MAP *map[UCHAR_MAX];
-MAPLIST mhead;
 
 /*
  * map_init --
@@ -36,9 +30,6 @@ map_init()
 {
 	int rval;
 
-	/* Link the linked list together. */
-	mhead.lnext = mhead.lprev = (MAP *)&mhead;
-
 	/*
 	 * The KU, KD, KL,and KR variables correspond to the :ku=: etc.
 	 * termcap capabilities.  The variables are defined as part of
@@ -46,25 +37,25 @@ map_init()
 	 */
 	rval = 0;
 	if (has_KU)
-		rval |= map_set("up",		has_KU,    "k", COMMAND, 0);
+		rval |= seq_set("up", has_KU, "k", COMMAND, 0);
 	if (has_KD)
-		rval |= map_set("down",		has_KD,    "j", COMMAND, 0);
+		rval |= seq_set("down", has_KD, "j", COMMAND, 0);
 	if (has_KL)
-		rval |= map_set("left",		has_KL,    "h", COMMAND, 0);
+		rval |= seq_set("left", has_KL, "h", COMMAND, 0);
 	if (has_KR)
-		rval |= map_set("right",	has_KR,    "l", COMMAND, 0);
+		rval |= seq_set("right", has_KR, "l", COMMAND, 0);
 	if (has_HM)
-		rval |= map_set("home",		has_HM,    "^", COMMAND, 0);
+		rval |= seq_set("home", has_HM, "^", COMMAND, 0);
 	if (has_EN)
-		rval |= map_set("end",		has_EN,    "$", COMMAND, 0);
+		rval |= seq_set("end", has_EN, "$", COMMAND, 0);
 	if (has_PU)
-		rval |= map_set("page up",	has_PU, "\002", COMMAND, 0);
+		rval |= seq_set("page up", has_PU, "\002", COMMAND, 0);
 	if (has_PD)
-		rval |= map_set("page down",	has_PD, "\006", COMMAND, 0);
+		rval |= seq_set("page down", has_PD, "\006", COMMAND, 0);
 	if (has_KI)
-		rval |= map_set("insert",	has_KI,    "i", COMMAND, 0);
+		rval |= seq_set("insert", has_KI, "i", COMMAND, 0);
 	if (ERASEKEY != '\177')
-		rval |= map_set("delete",	"\177",    "x", COMMAND, 0);
+		rval |= seq_set("delete", "\177", "x", COMMAND, 0);
 	return (rval);
 }
 
@@ -76,14 +67,15 @@ int
 ex_map(cmdp)
 	CMDARG *cmdp;
 {
-	enum whenmapped when;
+	register char *input, *output;
+	enum seqtype stype;
 	int key;
-	char *name, *input, *output, buf[10];
+	char *name, *s, buf[10];
 
-	when = cmdp->flags & E_FORCE ? INPUT : COMMAND;
+	stype = cmdp->flags & E_FORCE ? INPUT : COMMAND;
 
 	if (cmdp->string == NULL) {
-		map_dump(when);
+		seq_dump(stype);
 		return (0);
 	}
 
@@ -115,9 +107,28 @@ ex_map(cmdp)
 			msg("This terminal has no %s key.", buf);
 			return (1);
 		}
-	} else
+	} else {
 		name = NULL;
-	return (map_set(name, input, output, when, 1));
+
+		/* Some single keys may not be remapped in command mode. */
+		if (stype == COMMAND && input[1] == '\0')
+			switch (input[0]) {
+			case '\n':
+				s = "\\n";
+				goto noremap;
+			case '\r':
+				s = "\\r";
+				goto noremap;
+			case '\033':
+				s = "^[";
+				goto noremap;
+			case ':':
+				s = ":";
+noremap:			msg("The %s character may not be remapped.", s);
+				return (1);
+			}
+	}
+	return (seq_set(name, input, output, stype, 1));
 }
 
 /*
@@ -128,235 +139,25 @@ int
 ex_unmap(cmdp)
 	CMDARG *cmdp;
 {
-	enum whenmapped when;
-	register MAP *mp;
-	register char *input;
+	char *input;
 
 	input = cmdp->argv[0];
-	when = cmdp->flags & E_FORCE ? INPUT : COMMAND;
-	for (mp = map[*input]; mp; mp = mp->next) {
-		if (when != mp->when)
-			continue;
-		if (!strcmp(mp->input, input)) {
-
-			/* Unlink out of the map array. */
-			if (mp->prev) {
-				if (mp->next) {
-					mp->next->prev = mp->prev;
-					mp->prev->next = mp->next;
-				} else
-					mp->prev->next = NULL;
-			} else if (mp->next) {
-				map[*input] = mp->next;
-				mp->next->prev = NULL;
-			} else
-				map[*input] = NULL;
-
-			/* Unlink out of the map list. */
-			mp->lprev->lnext = mp->lnext;
-			mp->lnext->lprev = mp->lprev;
-
-			/* Free up the space. */
-			if (mp->name)
-				free(mp->name);
-			free(mp->input);
-			free(mp->output);
-			free(mp);
-			return (0);
-		}
-	}
-	msg("\"%s\" was never mapped.", input);
-	return (1);
-}
-
-/*
- * map_set --
- *	Internal version to map a key.
- */
-static int
-map_set(name, input, output, when, userdef)
-	char *name, *input, *output;
-	enum whenmapped when;
-	int userdef;
-{
-	register MAP *mp;
-	int ilen;
-	char *s;
-
-	ilen = strlen(input);
-	if (ilen == 1)
-		switch (input[0]) {	/* Some keys may not be remapped. */
-		case '\n':
-			s = "\\n";
-			goto nomap;
-		case '\r':
-			s = "\\r";
-			goto nomap;
-		case '\033':
-			s = "^[";
-			goto nomap;
-		case ':':
-			s = ":";
-nomap:			msg("The %s character may not be remapped.", s);
-			return (1);
-		}
-
-	/* Find any previous occurrence, and replace the output field. */
-	for (mp = map[*input]; mp; mp = mp->next) {
-		if (when != mp->when)
-			continue;
-		if (!strcmp(mp->input, input)) {
-			free(mp->output);
-			if ((mp->output = strdup(output)) == NULL)
-				goto mem1;
-			return (0);
-		}
-	}
-
-	/* Allocate space. */
-	if ((mp = malloc(sizeof(MAP))) == NULL) 
-		goto mem1;
-
-	if (name == NULL)
-		mp->name = NULL;
-	else 
-		if ((mp->name = strdup(name)) == NULL)
-			goto mem2;
-	if ((mp->input = strdup(input)) == NULL)
-		goto mem3;
-	if ((mp->output = strdup(output)) == NULL)
-		goto mem4;
-
-	mp->when = when;
-	mp->ilen = ilen;
-	mp->flags |= userdef ? M_USERDEF : 0;
-
-	/* Link into the map array. */
-	mp->next = map[*input];
-	map[*input] = mp;
-	mp->prev = NULL;
-
-	/* Link into the map list. */
-	mp->lnext = mhead.lnext;
-	mhead.lnext->lprev = mp;
-	mhead.lnext = mp;
-	mp->lprev = (MAP *)&mhead;
-	return (0);
-
-mem4:	free(mp->input);
-mem3:	if (mp->name)
-		free(mp->name);
-mem2:	free(mp);
-mem1:	msg("Error: %s", strerror(errno));
-	return (1);
-}
-
-/*
- * map_dump --
- *	Display key mappings of a specified type.
- */
-int
-map_dump(when)
-	enum whenmapped when;
-{
-	register MAP *mp;
-	register int ch, len;
-	register char *p;
-
-	for (mp = mhead.lnext; mp != (MAP *)&mhead; mp = mp->lnext) {
-		if (when != mp->when)
-			continue;
-		for (p = mp->name ? mp->name : mp->input, len = 0;
-		    (ch = *p); ++p, ++len)
-			if (iscntrl(ch)) {
-				qaddch('^');
-				qaddch(ch ^ '@');
-			} else
-				qaddch(ch);
-		for (len = TAB - len % TAB; len; --len)
-			qaddch(' ');
-		for (p = mp->input, len = 0; (ch = *p); ++p, ++len)
-			if (iscntrl(ch)) {
-				qaddch('^');
-				qaddch(ch ^ '@');
-			} else
-				qaddch(ch);
-		for (len = TAB - len % TAB; len; --len)
-			qaddch(' ');
-		for (p = mp->output; (ch = *p); ++p)
-			if (iscntrl(ch)) {
-				qaddch('^');
-				qaddch(ch ^ '@');
-			} else
-				qaddch(ch);
-
-		addch('\n');
-		exrefresh();
+	if (seq_delete(input, cmdp->flags & E_FORCE ? INPUT : COMMAND)) {
+		msg("\"%s\" was never mapped.", input);
+		return (1);
 	}
 	return (0);
 }
 
 /*
  * map_save --
- *	Save the current mapped keys to a file.
+ *	Save the mapped sequences to a file.
  */
-int
 map_save(fp)
 	FILE *fp;
 {
-	register MAP *mp;
-	register int ch;
-	register char *p;
-
-	/* Write a map command for all keys the user defined. */
-	for (mp = mhead.lnext; mp != (MAP *)&mhead; mp = mp->lnext) {
-		if (!(mp->flags & M_USERDEF))
-			continue;
-		(void)putc('m', fp);
-		(void)putc('a', fp);
-		(void)putc('p', fp);
-		if (mp->when == INPUT)
-			(void)putc('!', fp);
-		(void)putc(' ', fp);
-		for (p = mp->input; ch = *p; ++p) {
-			if (!isprint(ch) || ch == '|')
-				(void)putc('\026', fp);
-			(void)putc(ch, fp);
-		}
-		(void)putc(' ', fp);
-		for (p = mp->output; ch = *p; ++p) {
-			if (!isprint(ch) || ch == '|')
-				(void)putc('\026', fp);		/* 026 == ^V */
-			(void)putc(ch, fp);
-		}
-	}
-	return (0);
-}
-
-/*
- * map_find --
- *	Search the map for a match to a buffer, partial matches count.
- */
-MAP *
-map_find(input, when, ispartialp)
-	u_char *input;
-	enum whenmapped when;
-	int *ispartialp;
-{
-	register MAP *mp;
-	register int len;
-
-	*ispartialp = 0;
-	len = strlen((char *)input);
-	for (mp = map[*input]; mp; mp = mp->next) {
-		if (when != mp->when)
-			continue;
-		if (!strncmp(mp->input, (char *)input, len))
-			if (len == mp->ilen) {
-				*ispartialp = 0;
-				return (mp);
-			} else
-				*ispartialp = 1;
-	}
-	return (NULL);
+	
+	if (seq_save(fp, "map ", COMMAND));
+		return (1);
+	return (seq_save(fp, "map! ", INPUT));
 }
