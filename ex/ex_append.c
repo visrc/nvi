41a18811f8b26e9ba3f684a0ea1099828be8ae04
bostic <bostic>
1992-04-05 17:16:32 +0000

    cleanup, add autoindent/! behavior

--- ex/ex_append.c
+++ ex/ex_append.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_append.c,v 5.3 1992/04/05 09:23:27 bostic Exp $ (Berkeley) $Date: 1992/04/05 09:23:27 $";
+static char sccsid[] = "$Id: ex_append.c,v 5.4 1992/04/05 17:16:32 bostic Exp $ (Berkeley) $Date: 1992/04/05 17:16:32 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -14,21 +14,23 @@ static char sccsid[] = "$Id: ex_append.c,v 5.3 1992/04/05 09:23:27 bostic Exp $ 
 
 #include "vi.h"
 #include "excmd.h"
+#include "options.h"
 #include "extern.h"
 
 enum which {APPEND, CHANGE};
-static void Xchange __P((CMDARG *, enum which));
+
+static void ca __P((CMDARG *, enum which));
 
 /*
  * ex_append (:address append)
- *	Append one ore more lines of new text after the specified line,
+ *	Append one or more lines of new text after the specified line,
  *	or the current line if no address is specified.
  */
 int
 ex_append(cmdp)
 	CMDARG *cmdp;
 {
-	Xchange(cmdp, APPEND);
+	ca(cmdp, APPEND);
 	return (0);
 }
 
@@ -36,43 +38,55 @@ int
 ex_change(cmdp)
 	CMDARG *cmdp;
 {
-	Xchange(cmdp, CHANGE);
+	ca(cmdp, CHANGE);
 	return (0);
 }
 
 static void
-Xchange(cmdp, cmd)
+ca(cmdp, cmd)
 	CMDARG *cmdp;
 	enum which cmd;
 {
 	MARK m;
-	long l;			/* Line counter. */
+	long l;
+	int set;
 
-	m = cmdp->addrcnt ? cmdp->addr1 : cursor;
+	/* The ! flag turns off autoindent for the command. */
+	if (cmdp->flags & E_FORCE) {
+		set = ISSET(O_AUTOINDENT);
+		UNSET(O_AUTOINDENT);
+	} else
+		set = 0;
 
+	m = cmdp->addr1;
 	ChangeText
 	{
 		/* If we're doing a change, delete the old version. */
 		if (cmd == CHANGE)
 			ex_delete(cmdp);
 
-		/* New lines start at the frommark line, or after it. */
+		/*
+		 * New lines start at the specified line for changes,
+		 * after it for appends.
+		 */
 		l = markline(m);
-		if (cmd != APPEND)
- 			l++;
+		if (cmd == APPEND)
+ 			++l;
 
-		/* Get lines until no more lines, or "." line, and insert. */
-		while (vgets('\0', tmpblk.c, BLKSIZE) >= 0) {
+		/* Insert lines until no more lines, or "." line. */
+		for (; vgets('\0', tmpblk.c, BLKSIZE) >= 0; ++l) {
 			addch('\n');
 			if (!strcmp(tmpblk.c, "."))
 				break;
 
 			(void)strcat(tmpblk.c, "\n");
 			add(MARK_AT_LINE(l), tmpblk.c);
-			l++;
 		}
 	}
 
-	/* On the odd chance that we're calling this from vi mode. */
+	/* This can be called from vi mode. */
 	redraw(MARK_UNSET, FALSE);
+
+	if (set)
+		SET(O_AUTOINDENT);
 }
