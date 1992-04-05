64d555131458782051e4ca7173780f2abe9f9385
bostic <bostic>
1992-04-05 09:24:46 +0000

    rewrite after the ex command breakup make ex returns return a status code

--- ex/ex_at.c
+++ ex/ex_at.c
@@ -6,57 +6,50 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_at.c,v 5.2 1992/04/04 10:02:28 bostic Exp $ (Berkeley) $Date: 1992/04/04 10:02:28 $";
+static char sccsid[] = "$Id: ex_at.c,v 5.3 1992/04/05 09:24:46 bostic Exp $ (Berkeley) $Date: 1992/04/05 09:24:46 $";
 #endif /* not lint */
 
 #include <sys/types.h>
+#include <limits.h>
 #include <stdio.h>
 
 #include "vi.h"
 #include "excmd.h"
 #include "extern.h"
 
-void
+int
 ex_at(cmdp)
 	CMDARG *cmdp;
 {
-	static	nest = FALSE;
-	int	result;
-	char	buf[MAXRCLEN];
-	char *extra;
+	static int lastbuf, recurse;
+	static char rstack[UCHAR_MAX];
+	int buf, len, rval;
+	char copy[1024];
 
-	extra = cmdp->argv[0];
-	/* don't allow nested macros */
-	if (nest)
-	{
-		msg("@ macros can't be nested");
-		return;
+	if ((buf = cmdp->buffer) == '\0' && (buf = lastbuf) == '\0') {
+		msg("No previous buffer to execute.");
+		return (1);
 	}
-	nest = TRUE;
-
-	/* require a buffer name */
-	if (*extra == '"')
-		extra++;
-	if (!*extra || !isascii(*extra) ||!islower(*extra))
-	{
-		msg("@ requires a cut buffer name (a-z)");
+		
+	if (recurse++) {
+		if (rstack[buf]) {
+			msg("Buffer %c already occurs in this command.");
+			return (1);
+		}
+		rstack[buf] = 1;
+	} else
+		bzero(rstack, UCHAR_MAX);
+			
+	len = cb2str(buf, copy, sizeof(copy));
+	if (len <= 0) {
+		msg("Buffer %c is empty.", buf);
+		return (1);
 	}
-
-	/* get the contents of the buffer */
-	result = cb2str(*extra, buf, (unsigned)(sizeof buf));
-	if (result <= 0)
-	{
-		msg("buffer \"%c is empty", *extra);
+	if (len >= sizeof buf) {
+		msg("Buffer %c is too large to execute.", buf);
+		return (1);
 	}
-	else if (result >= sizeof buf)
-	{
-		msg("buffer \"%c is too large to execute", *extra);
-	}
-	else
-	{
-		/* execute the contents of the buffer as ex commands */
-		exstring(buf, result, 1);
-	}
-
-	nest = FALSE;
+	rval = exstring(copy, len);
+	--recurse;
+	return (rval);
 }
