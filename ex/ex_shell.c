235edb38e0c9e1a6917e1dd18759d0d3f58812d1
bostic <bostic>
1992-04-28 15:03:26 +0000

    BSD curses

--- ex/ex_shell.c
+++ ex/ex_shell.c
@@ -6,10 +6,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_shell.c,v 5.5 1992/04/19 08:54:00 bostic Exp $ (Berkeley) $Date: 1992/04/19 08:54:00 $";
+static char sccsid[] = "$Id: ex_shell.c,v 5.6 1992/04/28 15:03:26 bostic Exp $ (Berkeley) $Date: 1992/04/28 15:03:26 $";
 #endif /* not lint */
 
 #include <sys/param.h>
+#include <termios.h>
+#include <curses.h>
+#include <unistd.h>
 #include <stdio.h>
 
 #include "vi.h"
@@ -18,7 +21,7 @@ static char sccsid[] = "$Id: ex_shell.c,v 5.5 1992/04/19 08:54:00 bostic Exp $ (
 #include "extern.h"
 
 /*
- * ex_shell (:sh[ell])
+ * ex_shell -- :sh[ell]
  *	Invoke the program named in the SHELL environment variable with
  *	the argument -i.
  */
@@ -26,14 +29,16 @@ int
 ex_shell(cmdp)
 	EXCMDARG *cmdp;
 {
+	struct termios t;
 	int rval;
 	char buf[MAXPATHLEN];
 
-	suspend_curses();
+	EX_PRSTART(0);
 
+	(void)tcgetattr(STDIN_FILENO, &t);
+	(void)tcsetattr(STDIN_FILENO, TCSADRAIN, &origtermio);
 	(void)snprintf(buf, sizeof(buf), "%s -i", PVAL(O_SHELL));
 	rval = system(buf);
-
-	resume_curses(mode == MODE_EX);
+	(void)tcsetattr(STDIN_FILENO, TCSAFLUSH, &t);
 	return (rval);
 }
