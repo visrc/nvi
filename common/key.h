dd319b2d34bd720f127af304dae5c24f2ae59fcc
bostic <bostic>
1992-04-28 13:39:43 +0000

    add GB_NLECHO flag, use termios

--- common/key.h
+++ common/key.h
@@ -4,20 +4,21 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: key.h,v 5.1 1992/04/18 18:33:39 bostic Exp $ (Berkeley) $Date: 1992/04/18 18:33:39 $
+ *	$Id: key.h,v 5.2 1992/04/28 13:39:43 bostic Exp $ (Berkeley) $Date: 1992/04/28 13:39:43 $
  */
 
 #define	GB_BS		0x01	/* Backspace erases past command. */
 #define	GB_ESC		0x02	/* Escape executes command. */
 #define	GB_NL		0x04	/* Leave the newline on. */
-#define	GB_OFF		0x08	/* Leave the first buffer character empty. */
+#define	GB_NLECHO	0x08	/* Echo the newline. */
+#define	GB_OFF		0x10	/* Leave the first buffer character empty. */
 
 #define	ISQ(off)	qb[(off)]
 #define	QINIT		bzero(qb, cblen);
 #define	QSET(off)	qb[(off)] = 1
 
 extern u_long cblen;		/* Buffer lengths. */
-extern u_char *qb;		/* Quote buffer. */
+extern char *qb;		/* Quote buffer. */
 
 extern u_char *atkeybuf;	/* Base of shared at buffer. */
 extern u_char *atkeyp;		/* Next char of shared at buffer. */
@@ -25,4 +26,4 @@ extern u_long atkeybuflen;	/* Remaining keys in shared at buffer. */
 
 char	*gb __P((int, u_int));
 int	 gb_inc __P((void));
-void	 gb_init __P((struct termios *));
+void	 gb_init __P((void));
