48ffcebbe186f31b111b1bea1fb6e1a34ead2be9
bostic <bostic>
1992-04-28 17:42:25 +0000

    move ex/vi print macros here from excmd.h, gb now returns lenght

--- common/key.h
+++ common/key.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: key.h,v 5.3 1992/04/28 13:48:52 bostic Exp $ (Berkeley) $Date: 1992/04/28 13:48:52 $
+ *	$Id: key.h,v 5.4 1992/04/28 17:42:25 bostic Exp $ (Berkeley) $Date: 1992/04/28 17:42:25 $
  */
 
 #define	ESCAPE		'\033'		/* Escape character. */
@@ -26,6 +26,57 @@ extern u_char *atkeybuf;		/* Base of shared at buffer. */
 extern u_char *atkeyp;			/* Next char of shared at buffer. */
 extern u_long atkeybuflen;		/* Remaining keys in shared @ buffer. */
 
-char	*gb __P((int, u_int));
+char	*gb __P((int, size_t *, u_int));
 int	 gb_inc __P((void));
 void	 gb_init __P((void));
+
+
+/*
+ * Vi makes the screen ready for ex to print, but there are special ways that
+ * information gets displayed.   The output overwrites some ex commands, so
+ * in that case we erase the command line, outputting a '\r' to guarantee the
+ * first column.  (We could theoretically lose if the command line has already
+ * wrapped, but this should only result in additional characters being sent to
+ * the terminal.)  All other initial output lines are preceded by a '\n'.
+ *
+ * XXX
+ * Currently, there's a bug.  The msg line isn't getting erased when the line
+ * is used again without an intervening repaint, so there can be garabage on
+ * the end of the line.  The fix is to change curses to support a "force this
+ * line to be written" semantic.
+ */
+/* Start the sequence. */
+#define	EX_PRSTART(overwrite) { \
+	if (mode == MODE_VI) { \
+		if (ex_prstate == PR_NONE) { \
+			if (overwrite) { \
+				while (ex_prerase--) { \
+					(void)putchar('\b'); \
+					(void)putchar(' '); \
+					(void)putchar('\b'); \
+				} \
+				(void)putchar('\r'); \
+				ex_prstate = PR_STARTED; \
+			} else { \
+				(void)putchar('\n'); \
+				ex_prstate = PR_PRINTED; \
+			} \
+		}  else if (ex_prstate == PR_STARTED) { \
+			(void)putchar('\n'); \
+			ex_prstate = PR_PRINTED; \
+		} \
+	} else \
+		(void)putchar('\n'); \
+}
+
+/* Print a newline. */
+#define	EX_PRNEWLINE { \
+	(void)putchar('\n'); \
+	ex_prstate = PR_PRINTED; \
+}
+
+/* Print a trailing newline, if necessary. */
+#define	EX_PRTRAIL { \
+	if (mode != MODE_VI || ex_prstate == PR_PRINTED) \
+		(void)putchar('\n'); \
+}
