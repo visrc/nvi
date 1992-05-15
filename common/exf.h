1a8698a70a3d9c12d6bbdc960b29c259de0af1c3
bostic <bostic>
1992-05-15 10:58:45 +0000

    move MARK stuff here, use right types for line/column number

--- common/exf.h
+++ common/exf.h
@@ -4,8 +4,11 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: exf.h,v 5.3 1992/05/07 12:45:30 bostic Exp $ (Berkeley) $Date: 1992/05/07 12:45:30 $
+ *	$Id: exf.h,v 5.4 1992/05/15 10:58:45 bostic Exp $ (Berkeley) $Date: 1992/05/15 10:58:45 $
  */
+
+#ifndef _EXF_H_
+#define	_EXF_H_
 
 #include <db.h>
 
@@ -23,11 +26,18 @@
 
 typedef struct exf {
 	struct exf *next, *prev;		/* Linked list of files. */
-	DB *db;					/* Underlying db structure. */
-	u_long lno;				/* Last line number. */
-	u_long cno;				/* Last column number. */
 	char *name;				/* File name. */
 	size_t nlen;				/* File name length. */
+	DB *db;					/* Underlying db structure. */
+
+#ifdef INTENDED_FOR_THE_FUTURE
+	WINDOW *win;				/* Window structure. */
+	u_long lines;				/* Lines in window. */
+#endif
+	recno_t lno;				/* Cursor line number. */
+	size_t cno;				/* Cursor column number. */
+	recno_t ntop;				/* New first window line. */
+	recno_t otop;				/* Current first window line. */
 
 #define	F_MODIFIED	0x001			/* File has been modified. */
 #define	F_NAMECHANGED	0x002			/* File name changed. */
@@ -64,17 +74,34 @@ typedef struct {
 	(p)->next = (EXF *)(hp); \
 }
 
+/*
+ * The MARK structure defines a position in the file.  Because of the
+ * different interfaces used by the db(3) package, curses, and users,
+ * the line number is 1 based, while the column number is 0 based.  The
+ * line number is of type recno_t, because that's the underlying type
+ * of the database.  The column number is of type size_t so that we can
+ * malloc a line.
+ */
+typedef struct {
+#define	OOBLNO		0			/* Out-of-band line number. */
+	recno_t lno;				/* Line number. */
+	size_t cno;				/* Column number. */
+} MARK;
+
+extern MARK cursor;				/* Cursor MARK. */
+
 int	 file_default __P((void));
 EXF	*file_first __P((void));
+char	*file_gline __P((EXF *, recno_t, size_t *));
 void	 file_init __P((void));
 int	 file_ins __P((EXF *, char *));
 int	 file_iscurrent __P((char *));
-char	*file_line __P((EXF *, u_long, size_t *));
 u_long	 file_lline __P((EXF *));
 EXF	*file_next __P((EXF *));
 EXF	*file_prev __P((EXF *));
 int	 file_set __P((int, char *[]));
+int	 file_sline __P((EXF *, recno_t, char *, size_t));
 int	 file_start __P((EXF *));
 int	 file_stop __P((EXF *, int));
 int	 file_sync __P((EXF *, int));
-
+#endif /* !_EXF_H_ */
