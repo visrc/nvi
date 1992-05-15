4b845e8a39b0d18713690d2c1e71266fe9a9a284
bostic <bostic>
1992-05-15 11:08:27 +0000

    add mark functions

--- ex/ex_mark.c
+++ ex/ex_mark.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_mark.c,v 5.5 1992/05/07 12:46:54 bostic Exp $ (Berkeley) $Date: 1992/05/07 12:46:54 $";
+static char sccsid[] = "$Id: ex_mark.c,v 5.6 1992/05/15 11:08:27 bostic Exp $ (Berkeley) $Date: 1992/05/15 11:08:27 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -20,22 +20,13 @@ int
 ex_mark(cmdp)
 	EXCMDARG *cmdp;
 {
-	char *mc;
-
 	if (cmdp->argc == 0) {
-		msg("No mark name specified.");
-		return (1);;
+		msg("No mark name; use 'a' to 'z'.");
+		return (1);
 	}
-
-	/*
-	 * Validate the name of the mark.  Valid mark names are lowercase
-	 * ascii characters.
-	 */
-	mc = cmdp->argv[0];
-	if (!isascii(*mc) || !islower(*mc) || mc[1]) {
-		msg("Invalid mark name: %s", mc);
-		return (1);;
+	if (cmdp->argv[1]) {
+		msg("Invalid mark name; use 'a' to 'z'.");
+		return (1);
 	}
-	mark[*mc - 'a'] = cmdp->addr2;
-	return (0);
+	return (mark_set(cmdp->argv[0], &cmdp->addr2));
 }
