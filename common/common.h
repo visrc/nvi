2978ffc454087c3cd6f78c1ea652ba252fb5fdba
bostic <bostic>
1992-05-15 11:03:08 +0000

    move MARK stuff into exf.h

--- common/common.h
+++ common/common.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: common.h,v 5.22 1992/05/07 12:45:55 bostic Exp $ (Berkeley) $Date: 1992/05/07 12:45:55 $
+ *	$Id: common.h,v 5.23 1992/05/15 11:03:08 bostic Exp $ (Berkeley) $Date: 1992/05/15 11:03:08 $
  */
 
 #ifndef TRUE					/* XXX -- Delete. */
@@ -14,6 +14,7 @@
 #define FALSE	0
 #endif
 
+#define	ESCAPE		'\033'			/* Escape character. */
 
 #define	TAB	8				/* XXX -- Settable? */
 #define	REG	register			/* XXX -- Delete. */
@@ -21,16 +22,6 @@
 #define	inword(ch)	(isalnum(ch) || (ch) == '_')
 
 #define INFINITY	2000000001L	/* a very large integer */
-
-typedef struct {
-#define	OOBLC		0			/* Out-of-band line/column. */
-	u_long lno;				/* Line number. */
-	u_long cno;				/* Column number. */
-} MARK;
-extern MARK cursor;				/* Current position. */
-
-#define NMARKS	29
-extern MARK	mark[NMARKS];	/* marks a-z, plus mark ' and two temps */
 
 /*------------------------------------------------------------------------*/
 /* misc housekeeping variables & functions				  */
@@ -78,6 +69,3 @@ extern char	digraph();
 #define WHEN_MARK	0x0100	/* getkey: getting a mark name */
 #define WHEN_CHAR	0x0200	/* getkey: getting a destination for f/F/t/T */
 #define WHENMASK	(WHEN_VICMD|WHEN_VIINP|WHEN_VIREP|WHEN_REP1|WHEN_CUT|WHEN_MARK|WHEN_CHAR)
-
-extern MARK	*V_from;
-extern int	V_linemd;
