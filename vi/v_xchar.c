b48a32118dde92c1be7eecdda2a065588f80a0fb
bostic <bostic>
1992-05-17 17:11:54 +0000

    make x,X work in the new vi loop structure

--- vi/v_xchar.c
+++ vi/v_xchar.c
@@ -6,54 +6,104 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_xchar.c,v 5.4 1992/05/15 11:14:26 bostic Exp $ (Berkeley) $Date: 1992/05/15 11:14:26 $";
+static char sccsid[] = "$Id: v_xchar.c,v 5.5 1992/05/17 17:11:54 bostic Exp $ (Berkeley) $Date: 1992/05/17 17:11:54 $";
 #endif /* not lint */
 
 #include <sys/types.h>
-#include <stddef.h>
+#include <limits.h>
+#include <stdio.h>
 
 #include "vi.h"
 #include "vcmd.h"
+#include "cut.h"
+#include "options.h"
 #include "extern.h"
+
+#define	EMPTY {								\
+	bell();								\
+	if (ISSET(O_VERBOSE))						\
+		msg("No characters to delete.");			\
+	return (1);							\
+}
 
 /*
  * v_xchar --
- *	Deletes the character(s) that the cursor is on.
+ *	Deletes the character(s) on which the cursor sits.
  */
-MARK *
-v_xchar(m, cnt, cmd)
-	MARK	*m;	/* where to start deletions */
-	long	cnt;	/* number of chars to delete */
-	int	cmd;	/* either 'x' or 'X' */
+int
+v_xchar(vp, cp, rp)
+	VICMDARG *vp;
+	MARK *cp, *rp;
 {
-	MARK t;
+	MARK fm, tm;
+	u_long cnt;
 	size_t len;
+	char *p;
 
-	SETDEFCNT(1);
+	EGETLINE(p, cp->lno, len);
+	if (len == 0)
+		EMPTY;
 
-	/* for 'X', adjust so chars are deleted *BEFORE* cursor */
-	if (cmd == 'X')
-	{
-		if (m->cno < cnt)
-			return NULL;
-		m->cno -= cnt;
+	cnt = vp->flags & VC_C1SET ? vp->count : 1;
+	fm.lno = tm.lno = cp->lno;
+
+	/*
+	 * Try deleting from the cursor to the end of the line, w/o moving the
+	 * cursor.  If that's not enough, start deleting left.  The historic
+	 * version of vi didn't handle this reasonably; "4x" at EOL wasn't the
+	 * same as "xxxx" because the left movement of the cursor as part of
+	 * the 'x' command wasn't taken into account.
+	 */
+	if (cnt < len - cp->cno) {
+		fm.cno = cp->cno;
+		tm.cno = cp->cno + cnt;
+	} else {
+		tm.cno = len;
+		cnt -= len - cp->cno;
+		if (cnt > cp->cno)
+			fm.cno = 0;
+		else
+			fm.cno = cp->cno - cnt;
 	}
 
-	/* make sure we don't try to delete more thars than there are */
-	(void)file_gline(curf, m->lno, &len);
-	if (m->cno + cnt > len)
-	{
-		cnt = len - m->cno;
-	}
-	if (cnt == 1)
-	{
-		return NULL;
-	}
+	if (cut(vp->buffer == OOBCB ? DEFCB : vp->buffer, &fm, &tm, 0) ||
+	    delete(&fm, &tm, 0))
+		return (1);
 
-	/* do it */
-	t.lno = m->lno;
-	t.cno = m->cno + cnt;
-	cut(m, &t);
-	delete(m, &t);
-	return m;
+	*rp = fm;
+	return (0);
+}
+
+/*
+ * v_Xchar --
+ *	Deletes the character(s) immediately before the current cursor
+ *	position.
+ */
+int
+v_Xchar(vp, cp, rp)
+	VICMDARG *vp;
+	MARK *cp, *rp;
+{
+	MARK fm, tm;
+	u_long cnt;
+	size_t len;
+	char *p;
+
+	if (cp->cno == 0)
+		EMPTY;
+	EGETLINE(p, cp->lno, len);
+	if (len == 0)
+		EMPTY;
+
+	cnt = vp->flags & VC_C1SET ? vp->count : 1;
+	fm.lno = tm.lno = cp->lno;
+	fm.cno = cnt >= cp->cno ? 0 : cp->cno - cnt;
+	tm.cno = cp->cno;
+
+	if (cut(vp->buffer == OOBCB ? DEFCB : vp->buffer, &fm, &tm, 0) ||
+	    delete(&fm, &tm, 0))
+
+	rp->lno = fm.lno;
+	rp->cno = fm.cno;
+	return (0);
 }
