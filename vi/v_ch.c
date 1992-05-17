3ad6fddd065ec61857b822c89c002db89a153d90
bostic <bostic>
1992-05-17 14:25:50 +0000

    add a Berkeley header

--- vi/v_ch.c
+++ vi/v_ch.c
@@ -1,3 +1,14 @@
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "$Id: v_ch.c,v 5.9 1992/05/17 14:25:50 bostic Exp $ (Berkeley) $Date: 1992/05/17 14:25:50 $";
+#endif /* not lint */
+
 #include <sys/types.h>
 #include <stdio.h>
 
