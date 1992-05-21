7d3eece763a5b36f04f60a48841637cfce774d3e
bostic <bostic>
1992-05-21 12:55:39 +0000

    new version of cut/put code

--- ex/ex_put.c
+++ ex/ex_put.c
@@ -6,34 +6,34 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_put.c,v 5.7 1992/05/07 12:47:02 bostic Exp $ (Berkeley) $Date: 1992/05/07 12:47:02 $";
+static char sccsid[] = "$Id: ex_put.c,v 5.8 1992/05/21 12:55:39 bostic Exp $ (Berkeley) $Date: 1992/05/21 12:55:39 $";
 #endif /* not lint */
 
 #include <sys/types.h>
+#include <limits.h>
 #include <stdio.h>
 
 #include "vi.h"
 #include "excmd.h"
+#include "cut.h"
 #include "extern.h"
 
 int
 ex_put(cmdp)
 	EXCMDARG *cmdp;
 {
-	char *extra;
+	CB *cb;
+	int buffer;
 
-	extra = cmdp->argv[0];
-	/* choose your cut buffer */
-	if (*extra == '"')
-	{
-		extra++;
+	if (cmdp->buffer == OOBCB)
+		buffer = DEFCB;
+	else {
+		buffer = cmdp->buffer;
+		CBNAME(buffer, cb);
 	}
-	if (*extra)
-	{
-		cutname(*extra);
-	}
-
-	cursor = *paste(&cmdp->addr1, 1, 0);
+	
+	if (put(buffer, &cmdp->addr1, &cursor, 1))
+		return (1);
 
 	autoprint = 1;
 	return (0);
