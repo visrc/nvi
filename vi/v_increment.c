aa2be9b2a928acab77ee4f694030c6587193cf97
bostic <bostic>
1992-05-22 09:58:45 +0000

    make it really work

--- vi/v_increment.c
+++ vi/v_increment.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_increment.c,v 5.4 1992/05/15 11:14:12 bostic Exp $ (Berkeley) $Date: 1992/05/15 11:14:12 $";
+static char sccsid[] = "$Id: v_increment.c,v 5.5 1992/05/22 09:58:45 bostic Exp $ (Berkeley) $Date: 1992/05/22 09:58:45 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -15,6 +15,19 @@ static char sccsid[] = "$Id: v_increment.c,v 5.4 1992/05/15 11:14:12 bostic Exp 
 #include "vi.h"
 #include "vcmd.h"
 #include "extern.h"
+
+static int lastch = '+';
+static int lastcnt = 1;
+static char *fmt[] = {
+#define	DECIMAL	0
+	"%.*ld",
+#define	HEXC	1
+	"%#0.*lX",
+#define	HEXL	2
+	"%#0.*lx",
+#define	OCTAL	3
+	"%#0.*lo",
+};
 
 /*
  * v_increment -- [count]#[#+-]
@@ -25,75 +38,51 @@ v_increment(vp, cp, rp)
 	VICMDARG *vp;
 	MARK *cp, *rp;
 {
-	enum { DECIMAL, HEXC, HEXL, OCTAL} ntype;
 	MARK ecursor;
-	u_long val;
-	static int lastch = '+';
-	static int lastcnt = 1;
+	u_long ulval;
+	long lval;
 	size_t len;
-	char *p, *ep, nbuf[40];
+	char *p, *ntype, nbuf[40];
 
 	/* Do repeat operations. */
 	if (vp->character == '#')
 		vp->character = lastch;
 
 	/* Get new value. */
-	val = vp->flags & VC_C1SET ? vp->count : lastcnt;
-	switch(vp->character) {
-	case '+':
-		val += strtol(vp->keyword, &ep, 0);
-		break;
-	case '-':
-		val -= strtol(vp->keyword, &ep, 0);
-		break;
-	default:
+	if (vp->flags & VC_C1SET)
+		lastcnt = vp->count;
+
+	if (vp->character != '+' && vp->character != '-') {
 		bell();
 		msg("usage: %s.", vp->kp->usage);
 		return (1);
 	}
-
-	if (*ep) {
-		bell();
-		msg("Cursor not in a number.");
-		return (1);
-	}
+	lastch = vp->character;
 
 	/* Figure out the resulting type. */
 	p = vp->keyword;
 	len = vp->klen;
-	if (len > 1 && *p == '0') {
-		ntype = OCTAL;
-		++p;
-		--len;
-		if (len > 1)
-			if (*p == 'X') {
-				ntype = HEXC;
-				++p;
-				--len;
-			} else if (*p == 'x') {
-				ntype = HEXL;
-				++p;
-				--len;
-			}
-	} else
-		ntype = DECIMAL;
-
-	/* Build a new number. */
-	switch(ntype) {
-	case DECIMAL:
-		len = snprintf(nbuf, sizeof(nbuf), "%0.*lu", len, val);
-		break;
-	case HEXC:
-		len = snprintf(nbuf, sizeof(nbuf), "%#0.*lX", len, val);
-		break;
-	case HEXL:
-		len = snprintf(nbuf, sizeof(nbuf), "%#0.*lx", len, val);
-		break;
-	case OCTAL:
-		len = snprintf(nbuf, sizeof(nbuf), "%#0.*lo", len, val);
-		break;
+	if (len > 1 && p[0] == '0') {
+		if (vp->character == '+')
+			ulval = strtoul(vp->keyword, NULL, 0) + lastcnt;
+		else
+			ulval = strtoul(vp->keyword, NULL, 0) - lastcnt;
+		ntype = fmt[OCTAL];
+		if (len > 2)
+			if (p[1] == 'X')
+				ntype = fmt[HEXC];
+			else if (p[1] == 'x')
+				ntype = fmt[HEXL];
+		len = snprintf(nbuf, sizeof(nbuf), ntype, len, ulval);
+	} else {
+		if (vp->character == '+')
+			lval = strtol(vp->keyword, NULL, 0) + lastcnt;
+		else
+			lval = strtol(vp->keyword, NULL, 0) - lastcnt;
+		len = snprintf(nbuf, sizeof(nbuf), fmt[DECIMAL], len, lval);
 	}
 
+	*rp = *cp;
 	ecursor = *cp;
 	ecursor.cno += vp->klen;
 			
