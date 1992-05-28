f3a74f9f6dd1551a1f8bc0926fd3bd11ce05effb
bostic <bostic>
1992-05-28 13:47:56 +0000

    write v_Xchar, 'X' no longer a special case

--- vi/v_xchar.c
+++ vi/v_xchar.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_xchar.c,v 5.5 1992/05/17 17:11:54 bostic Exp $ (Berkeley) $Date: 1992/05/17 17:11:54 $";
+static char sccsid[] = "$Id: v_xchar.c,v 5.6 1992/05/28 13:47:56 bostic Exp $ (Berkeley) $Date: 1992/05/28 13:47:56 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -19,58 +19,51 @@ static char sccsid[] = "$Id: v_xchar.c,v 5.5 1992/05/17 17:11:54 bostic Exp $ (B
 #include "options.h"
 #include "extern.h"
 
-#define	EMPTY {								\
-	bell();								\
-	if (ISSET(O_VERBOSE))						\
-		msg("No characters to delete.");			\
-	return (1);							\
-}
-
 /*
  * v_xchar --
  *	Deletes the character(s) on which the cursor sits.
  */
 int
-v_xchar(vp, cp, rp)
+v_xchar(vp, fm, tm, rp)
 	VICMDARG *vp;
-	MARK *cp, *rp;
+	MARK *fm, *tm, *rp;
 {
-	MARK fm, tm;
+	MARK m;
 	u_long cnt;
 	size_t len;
 	char *p;
 
-	EGETLINE(p, cp->lno, len);
-	if (len == 0)
-		EMPTY;
-
-	cnt = vp->flags & VC_C1SET ? vp->count : 1;
-	fm.lno = tm.lno = cp->lno;
-
-	/*
-	 * Try deleting from the cursor to the end of the line, w/o moving the
-	 * cursor.  If that's not enough, start deleting left.  The historic
-	 * version of vi didn't handle this reasonably; "4x" at EOL wasn't the
-	 * same as "xxxx" because the left movement of the cursor as part of
-	 * the 'x' command wasn't taken into account.
-	 */
-	if (cnt < len - cp->cno) {
-		fm.cno = cp->cno;
-		tm.cno = cp->cno + cnt;
-	} else {
-		tm.cno = len;
-		cnt -= len - cp->cno;
-		if (cnt > cp->cno)
-			fm.cno = 0;
-		else
-			fm.cno = cp->cno - cnt;
+	EGETLINE(p, fm->lno, len);
+	if (len == 0) {
+		bell();
+		if (ISSET(O_VERBOSE))
+			msg("No characters to delete.");
+		return (1);
 	}
 
-	if (cut(vp->buffer == OOBCB ? DEFCB : vp->buffer, &fm, &tm, 0) ||
-	    delete(&fm, &tm, 0))
+	cnt = vp->flags & VC_C1SET ? vp->count : 1;
+	fm->lno = tm->lno = fm->lno;
+
+	/*
+	 * Deleting from the cursor toward the end of line, w/o moving the
+	 * cursor.  Note, "2x" at EOL isn't the same as "xx" because the
+	 * left movement of the cursor as part of the 'x' command is't taken
+	 * into account.  Historically correct.
+	 */
+	tm->lno = fm->lno;
+	if (cnt < len - fm->cno) {
+		tm->cno = fm->cno + cnt;
+		m = *fm;
+	} else {
+		tm->cno = len;
+		m.lno = fm->lno;
+		m.cno = fm->cno ? fm->cno - 1 : 0;
+	}
+
+	if (cut(VICB(vp), fm, tm, 0) || delete(fm, tm, 0))
 		return (1);
 
-	*rp = fm;
+	*rp = m;
 	return (0);
 }
 
@@ -80,30 +73,29 @@ v_xchar(vp, cp, rp)
  *	position.
  */
 int
-v_Xchar(vp, cp, rp)
+v_Xchar(vp, fm, tm, rp)
 	VICMDARG *vp;
-	MARK *cp, *rp;
+	MARK *fm, *tm, *rp;
 {
-	MARK fm, tm;
+	MARK m;
 	u_long cnt;
 	size_t len;
 	char *p;
 
-	if (cp->cno == 0)
-		EMPTY;
-	EGETLINE(p, cp->lno, len);
-	if (len == 0)
-		EMPTY;
+	if (fm->cno == 0) {
+		bell();
+		if (ISSET(O_VERBOSE))
+			msg("Already at the left-hand margin.");
+		return (1);
+	}
 
+	*tm = *fm;
 	cnt = vp->flags & VC_C1SET ? vp->count : 1;
-	fm.lno = tm.lno = cp->lno;
-	fm.cno = cnt >= cp->cno ? 0 : cp->cno - cnt;
-	tm.cno = cp->cno;
+	fm->cno = cnt >= tm->cno ? 0 : tm->cno - cnt;
 
-	if (cut(vp->buffer == OOBCB ? DEFCB : vp->buffer, &fm, &tm, 0) ||
-	    delete(&fm, &tm, 0))
+	if (cut(VICB(vp), fm, tm, 0) || delete(fm, tm, 0))
+			return (1);
 
-	rp->lno = fm.lno;
-	rp->cno = fm.cno;
+	*rp = *fm;
 	return (0);
 }
