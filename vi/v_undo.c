d53db48cfcb15633035ea2add159179115036e21
bostic <bostic>
1992-06-05 11:03:11 +0000

    new calling conventions

--- vi/v_undo.c
+++ vi/v_undo.c
@@ -6,27 +6,29 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_undo.c,v 5.6 1992/05/15 11:14:24 bostic Exp $ (Berkeley) $Date: 1992/05/15 11:14:24 $";
+static char sccsid[] = "$Id: v_undo.c,v 5.7 1992/06/05 11:03:11 bostic Exp $ (Berkeley) $Date: 1992/06/05 11:03:11 $";
 #endif /* not lint */
 
 #include <sys/types.h>
 
 #include "vi.h"
 #include "vcmd.h"
+#include "screen.h"
 #include "extern.h"
 
 /*
  * v_undo --
  *	Undoes the last change.
  */
-/* ARGSUSED */
-MARK *
-v_undo(m)
-	MARK *m;
+int
+v_undo(vp, fm, tm, rp)
+	VICMDARG *vp;
+	MARK *fm, *tm, *rp;
 {
 #ifdef NOT_RIGHT_NOW
 	if (undo())
 		scr_ref();
 #endif
-	return (&cursor);
+	*rp = *fm;
+	return (0);
 }
