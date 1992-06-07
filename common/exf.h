3205d312fbe37d3097781d3622cd42f37c186802
bostic <bostic>
1992-06-07 13:57:32 +0000

    add F_CREATED, F_WRITTEN; hacked scrolling code, first version of file stuff that mostly works; put cursor in EXF structure

--- common/exf.h
+++ common/exf.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: exf.h,v 5.6 1992/05/27 10:33:24 bostic Exp $ (Berkeley) $Date: 1992/05/27 10:33:24 $
+ *	$Id: exf.h,v 5.7 1992/06/07 13:57:32 bostic Exp $ (Berkeley) $Date: 1992/06/07 13:57:32 $
  */
 
 #ifndef _EXF_H_
@@ -12,37 +12,27 @@
 
 #include <db.h>
 
-/*
- * XXX
- * Old flags...
- */
-#define NEWFILE		0x0001	/* the file was just created */
-#define READONLY	0x0002	/* the file is read-only */
-#define HADNUL		0x0004	/* the file contained NUL characters */
-#define MODIFIED	0x0008	/* the file has been modified */
-#define NONAME		0x0010	/* no name is known for the current text */
-#define ADDEDNL		0x0020	/* newlines were added to the file */
-#define HADBS		0x0040	/* backspace chars were lost from the file */
-
 typedef struct exf {
 	struct exf *next, *prev;		/* Linked list of files. */
+
+						/* Screen state. */
+	recno_t lno;				/* Cursor line number. */
+	recno_t top;				/* Current first window line. */
+	size_t cno;				/* Cursor column number. */
+	size_t lcno;				/* Left column number. */
+	u_short scrollup;			/* Lines to scroll up. */
+	u_short scrolldown;			/* Lines to scroll down. */
+
 	char *name;				/* File name. */
 	size_t nlen;				/* File name length. */
 	DB *db;					/* Underlying db structure. */
 
-#ifdef INTENDED_FOR_THE_FUTURE
-	WINDOW *win;				/* Window structure. */
-	u_long lines;				/* Lines in window. */
-#endif
-	recno_t lno;				/* Cursor line number. */
-	size_t cno;				/* Cursor column number. */
-	recno_t ntop;				/* New first window line. */
-	recno_t otop;				/* Current first window line. */
-
-#define	F_MODIFIED	0x001			/* File has been modified. */
-#define	F_NAMECHANGED	0x002			/* File name changed. */
-#define	F_NONAME	0x004			/* File has no name. */
-#define	F_RDONLY	0x008			/* File is read-only. */
+#define	F_CREATED	0x001			/* File was created. */
+#define	F_MODIFIED	0x002			/* File was modified. */
+#define	F_NAMECHANGED	0x004			/* File name changed. */
+#define	F_NONAME	0x008			/* File has no name. */
+#define	F_RDONLY	0x010			/* File is read-only. */
+#define	F_WRITTEN	0x020			/* File has been written. */
 	u_int flags;
 } EXF;
 
@@ -92,13 +82,12 @@ typedef struct {
 }
 
 int	 file_aline __P((EXF *, recno_t, char *, size_t));
-int	 file_default __P((void));
 int	 file_dline __P((EXF *, recno_t));
 EXF	*file_first __P((void));
 char	*file_gline __P((EXF *, recno_t, size_t *));
 int	 file_iline __P((EXF *, recno_t, char *, size_t));
 void	 file_init __P((void));
-int	 file_ins __P((EXF *, char *));
+int	 file_ins __P((EXF *, char *, int));
 int	 file_iscurrent __P((char *));
 u_long	 file_lline __P((EXF *));
 EXF	*file_next __P((EXF *));
