c2ca846052644d995f9691f1d94c3221c45baab3
bostic <bostic>
1992-10-10 13:34:59 +0000

    char * to u_char *

--- common/common.h
+++ common/common.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: common.h,v 5.26 1992/08/22 19:18:25 bostic Exp $ (Berkeley) $Date: 1992/08/22 19:18:25 $
+ *	$Id: common.h,v 5.27 1992/10/10 13:34:59 bostic Exp $ (Berkeley) $Date: 1992/10/10 13:34:59 $
  */
 
 #include <db.h>				/* XXX for rptlines, below */
@@ -13,6 +13,27 @@
 #define	TAB	8			/* XXX -- Settable? */
 
 enum direction { FORWARD, BACKWARD };	/* Direction type. */
+
+/*
+ * Most of the arrays, names, etc. in ex/vi are u_char's, since we want
+ * it to be 8-bit clean.  Several of the C-library routines take char *'s,
+ * though.  The following #defines try to handle this problem.
+ *
+ * XXX
+ * I think that the correct approach is to have a set of data types as
+ * follows:
+ *	typedef char 		CFNAME	(file name character)
+ *	typedef unsigned char	CDATA	(data character)
+ *	typedef unsigned char	CINPUT	(input, command character)
+ */
+#define	UATOI(a)		atoi((char *)(a))
+#define	URINDEX(a, b)		(u_char *)rindex((char *)(a), b)
+#define	USTRCMP(a, b)		strcmp((char *)(a), (char *)(b))
+#define	USTRDUP(a)		(u_char *)strdup((char *)(a))
+#define	USTRLEN(a)		strlen((char *)(a))
+#define	USTRNCMP(a, b, n)	strncmp((char *)(a), (char *)(b), n)
+#define	USTRPBRK(a, b)		(u_char *)strpbrk((char *)(a), (char *)(b))
+#define	USTRTOL(a, b, c)	strtol((char *)(a), (char **)(b), c)
 
 /* misc housekeeping variables & functions				  */
 
