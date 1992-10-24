b3d945bd76aef1d7f16761ff2ee9255075dbf590
bostic <bostic>
1992-10-24 14:27:57 +0000

    rewrite for the new structure, z command now works

--- vi/v_z.c
+++ vi/v_z.c
@@ -1,85 +1,74 @@
-/* This file contains movement functions which are screen-relative */
+/*-
+ * Copyright (c) 1992 The Regents of the University of California.
+ * All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ */
+
+#ifndef lint
+static char sccsid[] = "$Id: v_z.c,v 5.18 1992/10/24 14:27:57 bostic Exp $ (Berkeley) $Date: 1992/10/24 14:27:57 $";
+#endif /* not lint */
 
 #include <sys/types.h>
 
-#include <curses.h>
 #include <limits.h>
-#include <stdio.h>
+#include <stddef.h>
 
 #include "vi.h"
-#include "options.h"
 #include "vcmd.h"
+#include "screen.h"
+#include "term.h"
 #include "extern.h"
 
-static MARK rval;
-
-/* This function repositions the current line to show on a given row */
-/*ARGSUSED*/
-MARK *m_z(m, cnt, key)
-	MARK	*m;	/* the cursor */
-	long	cnt;	/* the line number we're repositioning */
-	int	key;	/* key struck after the z */
+/*
+ * v_z -- [count]z[count][+.-]
+ *	Move the screen.
+ */
+int
+v_z(vp, fm, tm, rp)
+	VICMDARG *vp;
+	MARK *fm, *tm, *rp;
 {
-	long	newtop;
-	int	i;
+	recno_t lno;
 
-	/* Which line are we talking about? */
-	if (cnt < 0 || cnt > file_lline(curf))
-	{
-		return NULL;
+	if (vp->flags & VC_C1SET) {
+		fm->lno = vp->count;
+		if (file_gline(curf, fm->lno, NULL) == NULL)
+			fm->lno = file_lline(curf);
 	}
-	if (cnt)
-	{
-		m->lno = cnt;
-		newtop = cnt;
-	}
-	else
-	{
-		newtop = m->lno;
-	}
+	lno = fm->lno;
 
-	/* allow a "window size" number to be entered */
-	for (i = 0; key >= '0' && key <= '9'; key = getkey(0))
-	{
-		i = i * 10 + key - '0';
-	}
-	if (i > 0 && i <= LINES - 1)
-	{
-		LVAL(O_WINDOW) = i;
-	}
+	/*
+	 * XXX
+	 * The second count is the window size.  This needs to be
+	 * implemented when the general resizing routines are done.
+	 */
 
-	/* figure out which line will have to be at the top of the screen */
-	switch (key)
-	{
-	  case '\n':
-	  case '\r':
-	  case '+':
+	switch(vp->character) {
+	case '.':
+		if (lno <= HALFSCREEN(curf))
+			break;
+		lno -= HALFSCREEN(curf);
+		if (file_gline(curf, lno, NULL) == NULL)
+			break;
+		curf->top = lno;
 		break;
-
-	  case '.':
-	  case 'z':
-		newtop -= LINES / 2;
+	case '-':
+		if (lno <= SCREENSIZE(curf))
+			break;
+		lno -= SCREENSIZE(curf) - 1;
+		if (file_gline(curf, lno, NULL) == NULL)
+			break;
+		curf->top = lno;
 		break;
-
-	  case '-':
-		newtop -= LINES - 1;
-		break;
-
-	  default:
-		return NULL;
+	default:
+		if (special[vp->character] == K_CR) {
+			curf->top = lno;
+			break;
+		}
+		msg("usage: %s.", vp->kp->usage);
+		return (1);
 	}
-
-	/* make the new topline take effect */
-	if (newtop >= 1)
-	{
-		curf->top = newtop;
-	}
-	else
-	{
-		curf->top = 1L;
-	}
-
-	/* The cursor doesn't move */
-	rval = *m;
-	return (&rval);
+	*rp = *fm;
+	return (0);
 }
