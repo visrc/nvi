a6e36499f3f21d6e2bebb512622505b9b3928bd4
bostic <bostic>
1992-11-01 13:56:38 +0000

    set the return cursor value

--- vi/v_yank.c
+++ vi/v_yank.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_yank.c,v 5.9 1992/10/29 14:47:41 bostic Exp $ (Berkeley) $Date: 1992/10/29 14:47:41 $";
+static char sccsid[] = "$Id: v_yank.c,v 5.10 1992/11/01 13:56:38 bostic Exp $ (Berkeley) $Date: 1992/11/01 13:56:38 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -27,5 +27,6 @@ v_yank(vp, fm, tm, rp)
 	VICMDARG *vp;
 	MARK *fm, *tm, *rp;
 {
+	*rp = *fm;
 	return (cut(VICB(vp), fm, tm, vp->flags & VC_LMODE));
 }
