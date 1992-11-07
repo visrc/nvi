96e705c625782dcfcba3e76a8c5ee05f986179f7
bostic <bostic>
1992-11-07 18:44:27 +0000

    replace elvis with new version of the global code

--- ex/ex_global.c
+++ ex/ex_global.c
@@ -6,135 +6,184 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_global.c,v 5.13 1992/10/29 14:39:15 bostic Exp $ (Berkeley) $Date: 1992/10/29 14:39:15 $";
+static char sccsid[] = "$Id: ex_global.c,v 5.14 1992/11/07 18:44:27 bostic Exp $ (Berkeley) $Date: 1992/11/07 18:44:27 $";
 #endif /* not lint */
 
 #include <sys/types.h>
 
+#include <ctype.h>
 #include <limits.h>
-#include <regexp.h>
 #include <stdio.h>
+#include <string.h>
 
 #include "vi.h"
 #include "excmd.h"
+#include "options.h"
+#include "search.h"
 #include "extern.h"
 
 enum which {GLOBAL, VGLOBAL};
-static void global __P((EXCMDARG *, enum which));
+static int global __P((EXCMDARG *, enum which));
 
-int
-ex_vglobal(cmdp)
-	EXCMDARG *cmdp;
-{
-	global(cmdp, VGLOBAL);
-	return (0);
-}
-
+/*
+ * ex_global -- [line [,line]] g[lobal][!] /pattern/ [commands]
+ *	Exec on lines matching a pattern.
+ */
 int
 ex_global(cmdp)
 	EXCMDARG *cmdp;
 {
-	global(cmdp, VGLOBAL);
-	return (0);
+	return (global(cmdp, GLOBAL));
 }
 
-static void
+/*
+ * ex_vglobal -- [line [,line]] v[global] /pattern/ [commands]
+ *	Exec on lines not matching a pattern.
+ */
+int
+ex_vglobal(cmdp)
+	EXCMDARG *cmdp;
+{
+	return (global(cmdp, VGLOBAL));
+}
+
+static int
 global(cmdp, cmd)
 	EXCMDARG *cmdp;
 	enum which cmd;
 {
-	u_char	*cmdptr;	/* the command from the command line */
-	char	cmdln[100];	/* copy of the command from the command line */
-	char	*line;		/* a line from the file */
-	long	l;		/* used as a counter to move through lines */
-	long	lqty;		/* quantity of lines to be scanned */
-	long	nchanged;	/* number of lines changed */
-	int isv;
-	regexp	*re;		/* the compiled search expression */
-	u_char *extra;
+	recno_t elno, last1, last2, lno, nchanged;
+	regmatch_t match[1];
+	regex_t *re, lre;
+	size_t len;
+	int eval, reflags, rval;
+	u_char *ep, *ptrn, *s, cbuf[512];
+	char delim[2];
 
-/*
- * XXX
- * If no addresses, do all addresses.
- */
-	extra = cmdp->argv[0];
-
-	/* can't nest global commands */
-	if (doingglobal)
-	{
-		msg("Can't nest global commands.");
-		curf->rptlines = -1L;
-		return;
+	if (FF_ISSET(curf, F_IN_GLOBAL)) {
+		msg("Nested global commands aren't permitted.");
+		return (1);
 	}
 
-	/* XXX ??? ":g! ..." is the same as ":v ..." */
-	if (cmdp->flags & E_FORCE)
-		cmd = VGLOBAL;
+	/* Skip whitespace. */
+	for (s = cmdp->string; *s && isspace(*s); ++s);
 
-	/* make sure we got a search pattern */
-	if (*extra != '/' && *extra != '?')
-	{
-		msg("Usage: %c /regular expression/ command", cmd == VGLOBAL ? 'v' : 'g');
-		return;
+	/* Get delimiter. */
+	if (*s != '/' && *s != ';') {
+		msg("Usage: %s.", cmdp->cmd->usage);
+		return (1);
 	}
 
-	/* parse & compile the search pattern */
-	cmdptr = parseptrn(extra);
-	if (!extra[1])
-	{
-		msg("Can't use empty regular expression with '%c' command", cmd == VGLOBAL ? 'v' : 'g');
-		return;
+	/* Delimiter is the first character. */
+	delim[0] = s[0];
+	delim[1] = '\0';
+
+	/* Get the pattern string. */
+	ep = s + 1;
+	ptrn = USTRSEP(&ep, delim);
+
+	/* Get the command string. */
+	if (*ep == NULL) {
+		msg("No command string specified.");
+		return (1);
 	}
-	re = regcomp(extra + 1);
-	if (!re)
-	{
-		/* regcomp found & described an error */
-		return;
-	}
 
-	/* for each line in the range */
-	doingglobal = 1;
-	isv = cmd == VGLOBAL;
-#ifdef notdef
-	ChangeText
-	{
-		/* NOTE: we have to go through the lines in a forward order,
-		 * otherwise "g/re/p" would look funny.  *BUT* for "g/re/d"
-		 * to work, simply adding 1 to the line# on each loop won't
-		 * work.  The solution: count lines relative to the end of
-		 * the file.  Think about it.
-		 */
-		for (l = file_lline(curf) - cmdp->addr1.lno,
-			lqty = cmdp->addr2.lno - cmdp->addr1.lno + 1L,
-			nchanged = 0L;
-		     lqty > 0 && file_lline(curf) - l >= 0 && nchanged >= 0L;
-		     l--, lqty--)
-		{
-			/* fetch the line */
-			line = file_gline(curf, file_lline(curf) - l, NULL);
-
-			/* if it contains the search pattern... */
-			if ((!regexec(re, line, 1)) == isv)
-			{
-				/* move the cursor to that line */
-				curf->lno = file_lline(curf) - l;
-
-				/* do the ex command (without mucking up
-				 * the original copy of the command line)
-				 */
-				strcpy(cmdln, cmdptr);
-				curf->rptlines = 0L;
-				ex_cmd(cmdln);
-				nchanged += curf->rptlines;
-			}
+	/* If the substitute string is empty, use the last one. */
+	if (*ptrn == NULL) {
+		if (!FF_ISSET(curf, F_RE_SET)) {
+			msg("No previous regular expression.");
+			return (1);
 		}
+		re = &curf->sre;
+	} else {
+		/* Set RE flags. */
+		reflags = 0;
+		if (ISSET(O_EXTENDED))
+			reflags |= REG_EXTENDED;
+		if (ISSET(O_IGNORECASE))
+			reflags |= REG_ICASE;
+
+		/* Compile the RE. */
+		re = &lre;
+		if (eval = regcomp(re, (char *)ptrn, reflags)) {
+			re_error(eval, re);
+			return (1);
+		}
+
+		/* Set saved RE. */
+		curf->sre = lre;
+		FF_SET(curf, F_RE_SET);
 	}
-#endif
-	doingglobal = 0;
 
-	/* free the regexp */
-	free(re);
+	rval = 0;
+	nchanged = 0;
+	FF_SET(curf, F_IN_GLOBAL);
 
-	/* Reporting...*/
-	curf->rptlines = nchanged;
+	/* For each line... */
+	for (lno = cmdp->addr1.lno,
+	    elno = cmdp->addr2.lno; lno <= elno; ++lno) {
+
+		/* Get the line. */
+		if ((s = file_gline(curf, lno, &len)) == NULL) {
+			GETLINE_ERR(lno);
+			rval = 1;
+			goto err;
+		}
+
+		/* Search for a match. */
+		match[0].rm_so = 0;
+		match[0].rm_eo = len;
+		switch(eval = regexec(re, (char *)s, 1, match, REG_STARTEND)) {
+		case 0:
+			if (cmd == VGLOBAL)
+				continue;
+			break;
+		case REG_NOMATCH:
+			if (cmd == GLOBAL)
+				continue;
+			break;
+		default:
+			re_error(eval, re);
+			rval = 1;
+			goto err;
+		}
+
+		/*
+		 * Execute the command, keeping track of the lines that
+		 * change, and adjusting for inserted/deleted lines.
+		 */
+		curf->rptlines = 0;
+		last1 = file_lline(curf);
+
+		(void)snprintf((char *)cbuf, sizeof(cbuf), "%d %s", lno, ep);
+		if (ex_cmd(cbuf)) {
+			rval = 1;
+			goto err;
+		}
+
+		last2 = file_lline(curf);
+		if (last2 > last1) {
+			last2 -= last1;
+			lno += last2;
+			elno += last2;
+		} else if (last1 > last2) {
+			last1 -= last2;
+			lno -= last1;
+			elno -= last1;
+		}
+
+		/* Cursor moves to last line sent to command. */
+		curf->lno = lno;
+
+		/* Cumulative line change count. */
+		nchanged += curf->rptlines;
+		curf->rptlines = 0;
+	}
+
+	/* Report statistics. */
+err:	curf->rptlines += nchanged;
+	curf->rptlabel = "changed";
+
+	FF_CLR(curf, F_IN_GLOBAL);
+	return (rval);
 }
