650e7d14f567f7ed925450a5f2815c51155d3f4d
bostic <bostic>
1993-04-12 14:35:29 +0000

    rework input processing: doubly link and change TEXT, delete IB

--- ex/ex_display.c
+++ ex/ex_display.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_display.c,v 5.7 1993/04/05 07:11:28 bostic Exp $ (Berkeley) $Date: 1993/04/05 07:11:28 $";
+static char sccsid[] = "$Id: ex_display.c,v 5.8 1993/04/12 14:35:29 bostic Exp $ (Berkeley) $Date: 1993/04/12 14:35:29 $";
 #endif /* not lint */
 
 #include <ctype.h>
@@ -30,11 +30,12 @@ ex_bdisplay(sp, ep, cmdp)
 	int cnt;
 
 	for (cb = sp->cuts, cnt = 0; cnt < UCHAR_MAX; ++cb, ++cnt) {
-		if (cb->head == NULL)
+		if (cb->txthdr.next == NULL || cb->txthdr.next != &cb->txthdr)
 			continue;
 		db(sp, charname(sp, cnt), cb);
 	}
-	if (sp->cuts[DEFCB].head != NULL)
+	if (sp->cuts[DEFCB].txthdr.next != NULL ||
+	    sp->cuts[DEFCB].txthdr.next != &sp->cuts[DEFCB].txthdr)
 		db(sp, "default buffer", &sp->cuts[DEFCB]);
 	return (0);
 }
@@ -49,6 +50,6 @@ db(sp, name, cb)
 
 	(void)fprintf(sp->stdfp, "%s%s\n", name,
 	    cb->flags & CB_LMODE ? ": line mode" : "");
-	for (tp = cb->head; tp; tp = tp->next)
-		(void)fprintf(sp->stdfp, "%.*s\n", tp->len, tp->lp);
+	for (tp = cb->txthdr.next; tp != (TEXT *)&cb->txthdr; tp = tp->next)
+		(void)fprintf(sp->stdfp, "%.*s\n", (int)tp->len, tp->lb);
 }
