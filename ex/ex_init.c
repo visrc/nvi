feb7194041a298d008106f8456a5460d47b23c59
bostic <bostic>
1993-04-17 11:59:41 +0000

    break screen and ex editor apart more cleanly make start_lno, start_cno, F_NOSETPOS really work

--- ex/ex_init.c
+++ ex/ex_init.c
@@ -6,10 +6,13 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_init.c,v 5.12 1993/04/05 07:11:37 bostic Exp $ (Berkeley) $Date: 1993/04/05 07:11:37 $";
+static char sccsid[] = "$Id: ex_init.c,v 5.13 1993/04/17 11:59:41 bostic Exp $ (Berkeley) $Date: 1993/04/17 11:59:41 $";
 #endif /* not lint */
 
-#include <unistd.h>
+#include <sys/types.h>
+
+#include <stdlib.h>
+#include <string.h>
 
 #include "vi.h"
 #include "excmd.h"
@@ -23,23 +26,51 @@ ex_init(sp, ep)
 	SCR *sp;
 	EXF *ep;
 {
-	struct termios raw;
-
-	/* Initialize the terminal state. */
-	if (tcgetattr(STDIN_FILENO, &raw))
-		return (1);
-	cfmakeraw(&raw);
-	raw.c_oflag |= OPOST|ONLCR;
-	if (tcsetattr(STDIN_FILENO, TCSADRAIN, &raw))
-		return (1);
+	recno_t last;
+	size_t len;
 
 	/*
-	 * Ex always starts editing at the end of the file;
-	 * going to ex from vi retains the current line.
+	 * If no starting location specified, ex starts at the end.
+	 * Otherwise, check to make sure that the location exists.
 	 */
-	if (F_ISSET(ep, F_NEWSESSION))
+	if (F_ISSET(ep, F_NOSETPOS)) {
 		sp->lno = file_lline(sp, ep);
-	sp->cno = 0;
+		sp->cno = 0;
+	} else if (file_gline(sp, ep, ep->lno, &len) == NULL) {
+		last = file_lline(sp, ep);
+		if (last == 0)
+			last = 1;
+		if (sp->lno != last || sp->cno != 0) {
+			sp->lno = last;
+			sp->cno = 0;
+			msgq(sp, M_INFO,
+			    "Cursor position changed since last edit");
+		}
+	} else {
+		sp->lno = ep->lno;
+		if (ep->cno >= len) {
+			sp->cno = 0;
+			msgq(sp, M_INFO,
+			    "Cursor position changed since last edit");
+		} else
+			sp->cno = ep->cno;
+	}
+
+	/*
+	 * After location established, run any initial command.  Failure
+	 * doesn't halt the session.  Don't worry about the cursor being
+	 * repositioned affecting the success of this command, it's
+	 * pretty unlikely.
+	 */
+	if (F_ISSET(ep, F_ICOMMAND)) {
+		(void)ex_cstring(sp, ep, ep->icommand, strlen(ep->icommand));
+		free(ep->icommand);
+		F_CLR(ep, F_ICOMMAND);
+	}
+
+	/* Display the status line. */
+	status(sp, ep, sp->lno);
+
 	return (0);
 }
 
@@ -51,7 +82,8 @@ int
 ex_end(sp)
 	SCR *sp;
 {
-	/* Reset the terminal state. */
-	return (tcsetattr(STDIN_FILENO,
-	    TCSADRAIN, &sp->gp->original_termios) ? 1 : 0);
+	/* Save cursor location. */
+	sp->ep->lno = sp->lno;
+	sp->ep->cno = sp->cno;
+	return (0);
 }
