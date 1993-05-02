7040d48c74f70440c0f086a3af926dcd02693034
bostic <bostic>
1993-05-02 18:30:19 +0000

    check for past the EOL in all cases

--- vi/v_delete.c
+++ vi/v_delete.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_delete.c,v 5.20 1993/05/02 18:00:44 bostic Exp $ (Berkeley) $Date: 1993/05/02 18:00:44 $";
+static char sccsid[] = "$Id: v_delete.c,v 5.21 1993/05/02 18:30:19 bostic Exp $ (Berkeley) $Date: 1993/05/02 18:30:19 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -75,32 +75,29 @@ v_delete(sp, ep, vp, fm, tm, rp)
 		return (0);
 	}
 
-	/* If deleting lines, leave the cursor at the lowest line deleted. */
+	/*
+	 * If deleting lines, leave the cursor at the lowest line deleted,
+	 * else, leave the cursor where it started.  Always correct for EOL.
+	 *
+	 * The historic vi would delete the line the cursor was on (even if
+	 * not in line mode) if the motion was past the end-of-file and the
+	 * cursor didn't originate on the last line of the file.  We never
+	 * delete the line the cursor is on -- we'd have to pass a flag down
+	 * to the delete() routine which would have to special case it.
+	 */
 	if (lmode) {
 		rp->lno = MIN(fm->lno, tm->lno);
 		if (rp->lno > nlines)
 			rp->lno = nlines;
-		rp->cno = fm->cno;
-		return (0);
-	}
+	} else
+		rp->lno = fm->lno;
 
-	/*
-	 * Not line mode.  The historic vi would delete the line the cursor
-	 * was on if the motion was past the end-of-file and the cursor didn't
-	 * originate on the last line of the file.  We never delete the line
-	 * the cursor is on -- we'd have to pass a flag down to the delete()
-	 * routine and it would have to special case the action. If anyone
-	 * notices this and complains, don't pause, don't hesitate.  Kill them.
-	 *
-	 * Leave the cursor it where it started, but correct for EOL.
-	 */
-	rp->lno = fm->lno;
 	if (file_gline(sp, ep, rp->lno, &len) == NULL) {
 		GETLINE_ERR(sp, rp->lno);
 		return (1);
 	}
 	if (fm->cno >= len)
-		rp->cno = fm->cno ? fm->cno - 1 : 0;
+		rp->cno = len ? len - 1 : 0;
 	else
 		rp->cno = fm->cno;
 	return (0);
