b9de931331e47cd9ca59ad2ecb2d4605f65093ab
bostic <bostic>
1993-05-04 16:12:05 +0000

    document O_TERSE/O_VERBOSE rules

--- common/msg.h
+++ common/msg.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: msg.h,v 5.6 1993/04/17 11:50:04 bostic Exp $ (Berkeley) $Date: 1993/04/17 11:50:04 $
+ *	$Id: msg.h,v 5.7 1993/05/04 16:12:05 bostic Exp $ (Berkeley) $Date: 1993/05/04 16:12:05 $
  */
 
 /*
@@ -13,6 +13,11 @@
  * M_ERR	-- Error: display in inverse video.
  * M_INFO	-- Info: display in normal video.
  * M_VINFO	-- Info: display only if O_VERBOSE set.
+ *
+ * In historical vi, O_VERBOSE didn't exist, and O_TERSE made the
+ * error messages shorter.  In this version, O_TERSE has no effect
+ * and O_VERBOSE results in informational displays about common
+ * errors.
  */
 enum msgtype { M_BERR, M_ERR, M_INFO, M_VINFO };
 
