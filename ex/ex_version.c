96352f7c314ad283c6db6a1d2dc64cd1aa241ada
bostic <bostic>
1993-05-08 20:40:41 +0000

    bump to 0.2.1

--- ex/ex_version.c
+++ ex/ex_version.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_version.c,v 5.14 1993/05/06 00:34:58 bostic Exp $ (Berkeley) $Date: 1993/05/06 00:34:58 $";
+static char sccsid[] = "$Id: ex_version.c,v 5.15 1993/05/08 20:40:41 bostic Exp $ (Berkeley) $Date: 1993/05/08 20:40:41 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -25,6 +25,6 @@ ex_version(sp, ep, cmdp)
 	EXCMDARG *cmdp;
 {
 	(void)fprintf(sp->stdfp,
-	    "Version 0.2 (The CSRG, U.C. Berkeley.): %s", "$Date: 1993/05/06 00:34:58 $");
+	    "Version 0.2.1 (The CSRG, U.C. Berkeley.): %s", "$Date: 1993/05/08 20:40:41 $");
 	return (0);
 }
