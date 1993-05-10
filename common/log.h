a8a83d2ee8f9423e57a9627cd35cecd30ae6f6fc
bostic <bostic>
1993-05-10 11:14:17 +0000

    reimplement the logging subsystem

--- common/log.h
+++ common/log.h
@@ -4,42 +4,24 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: log.h,v 5.4 1993/03/26 13:37:48 bostic Exp $ (Berkeley) $Date: 1993/03/26 13:37:48 $
+ *	$Id: log.h,v 5.5 1993/05/10 11:14:17 bostic Exp $ (Berkeley) $Date: 1993/05/10 11:14:17 $
  */
 
-/*
- * The log consists of records, where a record contains a type byte and
- * then a variable length byte string, as follows:
- *
- *	LOG_CURSOR		MARK
- *	LOG_LINE_APPEND 	recno_t		line
- *	LOG_LINE_DELETE		recno_t		line
- *	LOG_LINE_INSERT		recno_t		line
- *	LOG_LINE_RESET		recno_t		line
- *	LOG_MARK		key		MARK
- *	LOG_START
- *
- * We do before image physical logging.  This means that the editor layer
- * cannot modify records in place, even if it's deleting or overwriting
- * characters.  Since the smallest unit of logging is a line, we're using
- * up lots of space.  This may eventually have to be reduced, probably by
- * doing logical logging, which is a much cooler database phrase.
- */
+#define	LOG_NOTYPE		0
+#define	LOG_CURSOR_INIT		1
+#define	LOG_CURSOR_END		2
+#define	LOG_LINE_APPEND		3
+#define	LOG_LINE_DELETE		4
+#define	LOG_LINE_INSERT		5
+#define	LOG_LINE_RESET_F	6
+#define	LOG_LINE_RESET_B	7
+#define	LOG_MARK		8
 
-#define	LOG_NOTYPE	0
-#define	LOG_CURSOR	1
-#define	LOG_LINE_APPEND	2
-#define	LOG_LINE_DELETE	3
-#define	LOG_LINE_INSERT	4
-#define	LOG_LINE_RESET	5
-#define	LOG_MARK	6
-#define	LOG_START	7
-
-int	log_backward __P((struct _scr *,
-	    struct _exf *, struct _mark *, recno_t));
+int	log_backward __P((struct _scr *, struct _exf *, struct _mark *));
 int	log_cursor __P((struct _scr *, struct _exf *));
 int	log_end __P((struct _scr *, struct _exf *));
 int	log_forward __P((struct _scr *, struct _exf *, struct _mark *));
 int	log_init __P((struct _scr *, struct _exf *));
 int	log_line __P((struct _scr *, struct _exf *, recno_t, u_int));
 int	log_mark __P((struct _scr *, struct _exf *, u_int, struct _mark *));
+int	log_setline __P((struct _scr *, struct _exf *));
