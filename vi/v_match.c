bdbda99c79b1419b501be3627b295483e36900b3
bostic <bostic>
1993-05-10 16:04:38 +0000

    delete matching to comments -- it's not useful enough to keep, and doesn't help the semantics of match much

--- vi/v_match.c
+++ vi/v_match.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_match.c,v 5.20 1993/05/08 10:52:43 bostic Exp $ (Berkeley) $Date: 1993/05/08 10:52:43 $";
+static char sccsid[] = "$Id: v_match.c,v 5.21 1993/05/10 16:04:38 bostic Exp $ (Berkeley) $Date: 1993/05/10 16:04:38 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -46,31 +46,6 @@ v_match(sp, ep, vp, fm, tm, rp)
 		goto nomatch;
 
 	switch (startc = p[fm->cno]) {
-	case '/':
-		matchc = '*';
-		if (fm->cno < len - 1 && p[fm->cno + 1] == '*') {
-			++fm->cno;
-			dir = FORWARD;
-			break;
-		}
-		if (fm->cno > 0 && p[fm->cno - 1] == '*') {
-			--fm->cno;
-			dir = BACKWARD;
-			break;
-		}
-		goto search;
-	case '*':
-		startc = '/';			/* Ignore startc. */
-		matchc = '*';
-		if (fm->cno < len - 1 && p[fm->cno + 1] == '/') {
-			dir = BACKWARD;
-			break;
-		}
-		if (fm->cno > 0 && p[fm->cno - 1] == '/') {
-			dir = FORWARD;
-			break;
-		}
-		goto search;
 	case '(':
 		matchc = ')';
 		dir = FORWARD;
@@ -96,9 +71,9 @@ v_match(sp, ep, vp, fm, tm, rp)
 		dir = BACKWARD;
 		break;
 	default:
-search:		if (F_ISSET(vp, VC_C | VC_D | VC_Y)) {
+		if (F_ISSET(vp, VC_C | VC_D | VC_Y)) {
 			msgq(sp, M_BERR,
-			"Proximity search doesn't work for motion commands.");
+			"Proximity match doesn't work for motion commands.");
 			return (1);
 		}
 		if (findmatchc(fm, p, len, rp)) {
@@ -111,19 +86,10 @@ nomatch:		msgq(sp, M_BERR, "No match character on this line.");
 	if (getc_init(sp, ep, fm, &ch))
 		return (1);
 	for (cnt = 1; getc_next(sp, ep, dir, &ch);)
-		if (ch == startc) {
-			if (startc != '/')
-				++cnt;
-		} else if (ch == matchc) {
-			if (ch == '*') {
-				if (!getc_next(sp, ep, dir, &ch))
-					break;
-				if (ch != '/')
-					continue;
-			}
-			if (--cnt == 0)
-				break;
-		} 
+		if (ch == startc)
+			++cnt;
+		else if (ch == matchc && --cnt == 0)
+			break;
 	if (cnt) {
 		msgq(sp, M_BERR, "Matching character not found.");
 		return (1);
@@ -145,12 +111,10 @@ nomatch:		msgq(sp, M_BERR, "No match character on this line.");
 /*
  * findmatchc --
  *	If we're not on a character we know how to match, try and find the
- *	closest character from the set "{}[]()".  (It might be useful to be
- *	able to enter % in the middle of the comment and find a match, but
- *	that would require searching across larger areas.)  The historic vi
- *	also looked for a character to match against, but in a completely
- *	inexplicable and apparently random fashion.  We search forward, then
- *	backward, and go to the closest one.  Ties go left for no reason.
+ *	closest character from the set "{}[]()".  The historic vi would also
+ *	look for a character to match against, but in an inexplicable and
+ *	apparently random fashion.  We search forward, then backward, and go
+ *	to the closest one.  Ties go left for no reason.
  */
 static int
 findmatchc(fm, p, len, rp)
@@ -164,35 +128,17 @@ findmatchc(fm, p, len, rp)
 	char *t;
 
 	leftfound = rightfound = 0;
-	for (off = 0, t = &p[off]; off++ < fm->cno; ++t)
-		if (strchr("/*{}[]()", t[0])) {
-			if (t[0] == '/') {
-				if (off == fm->cno || t[1] != '*')
-					continue;
-				left = off;
-			} else if (t[0] == '*') {
-				if (off == fm->cno || t[1] != '/')
-					continue;
-				left = off;
-			} else
-				left = off - 1;
+	for (off = 0, t = &p[off]; off++ < fm->cno;)
+		if (strchr("{}[]()", *t++)) {
+			left = off - 1;
 			leftfound = 1;
 			break;
 		}
 
 	--len;
-	for (off = fm->cno + 1, t = &p[off]; off++ < len; ++t)
-		if (strchr("/*{}[]()", t[0])) {
-			if (t[0] == '/') {
-				if (off == len || t[1] != '*')
-					continue;
-				right = off;
-			} else if (t[0] == '*') {
-				if (off == fm->cno || t[1] != '/')
-					continue;
-				right = off;
-			} else
-				right = off - 1;
+	for (off = fm->cno + 1, t = &p[off]; off++ < len;)
+		if (strchr("{}[]()", *t++)) {
+			right = off - 1;
 			rightfound = 1;
 			break;
 		}
