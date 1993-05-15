429238ca02858c3cb791ddbc645d4ef60f5978ee
bostic <bostic>
1993-05-15 10:55:21 +0000

    s_ex_run doesn't need a from/to MARK

--- vi/v_ex.c
+++ vi/v_ex.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_ex.c,v 5.51 1993/05/11 16:11:19 bostic Exp $ (Berkeley) $Date: 1993/05/11 16:11:19 $";
+static char sccsid[] = "$Id: v_ex.c,v 5.52 1993/05/15 10:55:21 bostic Exp $ (Berkeley) $Date: 1993/05/15 10:55:21 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -25,5 +25,5 @@ v_ex(sp, ep, vp, fm, tm, rp)
 	VICMDARG *vp;
 	MARK *fm, *tm, *rp;
 {
-	return (sp->s_ex_run(sp, ep, fm, tm, rp));
+	return (sp->s_ex_run(sp, ep, rp));
 }
