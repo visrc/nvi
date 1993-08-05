ca9c9d9ce1cb296f6c89c1da99c32f376a711e56
bostic <bostic>
1993-08-05 18:08:13 +0000

    make set_alt_fname general purpose, move to nvi/util.c

--- ex/ex_util.c
+++ ex/ex_util.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_util.c,v 8.1 1993/06/09 22:26:12 bostic Exp $ (Berkeley) $Date: 1993/06/09 22:26:12 $";
+static char sccsid[] = "$Id: ex_util.c,v 8.2 1993/08/05 18:08:13 bostic Exp $ (Berkeley) $Date: 1993/08/05 18:08:13 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -46,35 +46,4 @@ ex_getline(sp, fp, lenp)
 		*p++ = ch;
 	}
 	/* NOTREACHED */
-}
-
-/*
- * set_altfname --
- *	Set the alternate file name.
- *
- * Swap the alternate file name.  The reason it's a routine is that I wanted
- * some place to hang this comment.  The alternate file name (normally
- * referenced using the special character '#' during file expansion) is set
- * by a large number of operations.  In the historic vi, the commands "ex",
- * "edit" and "next file" obviously set the alternate file name because they
- * switched the underlying file.  Less obviously, the "read", "file", "write"
- * and "wq" commands set it as well.  In this implementation, the new
- * commands "previous", "split", and "mkexrc" have been added to the list.
- * Where it gets interesting is that the alternate file name is set multiple
- * times by some commands.  If an edit attempt fails (for whatever reason,
- * like the current file is modified but as yet unwritten), it is set to the
- * file name that the user didn't get to edit.  If the edit succeeds, it is
- * set to the last file name that was edited.  Good fun.
- *
- * XXX: add errlist if it doesn't get ripped out.
- */
-void
-set_altfname(sp, altfname)
-	SCR *sp;
-	char *altfname;
-{
-	if (sp->altfname != NULL)
-		FREE(sp->altfname, strlen(sp->altfname));
-	if ((sp->altfname = strdup(altfname)) == NULL)
-		msgq(sp, M_ERR, "Error: %s", strerror(errno));
 }
