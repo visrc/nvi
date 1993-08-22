a00cfc8f1c596723c497e39fb04a5dd312602415
bostic <bostic>
1993-08-22 10:46:46 +0000

    minor cleanup/speedup

--- vi/v_yank.c
+++ vi/v_yank.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_yank.c,v 8.5 1993/08/19 14:58:29 bostic Exp $ (Berkeley) $Date: 1993/08/19 14:58:29 $";
+static char sccsid[] = "$Id: v_yank.c,v 8.6 1993/08/22 10:46:46 bostic Exp $ (Berkeley) $Date: 1993/08/22 10:46:46 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -26,20 +26,18 @@ v_yank(sp, ep, vp, fm, tm, rp)
 	VICMDARG *vp;
 	MARK *fm, *tm, *rp;
 {
-	rp->lno = fm->lno;
-	rp->cno = fm->cno;
-
-	tm->lno = fm->lno + (F_ISSET(vp, VC_C1SET) ? vp->count - 1 : 0);
-
-	if (F_ISSET(vp, VC_LMODE)) {
+	if (F_ISSET(vp, VC_LMODE) && tm->lno != fm->lno) {
 		if (file_gline(sp, ep, tm->lno, NULL) == NULL) {
-			v_eof(sp, ep, rp);
+			v_eof(sp, ep, fm);
 			return (1);
 		}
 		if (cut(sp, ep, VICB(vp), fm, tm, 1))
 			return (1);
 	} else if (cut(sp, ep, VICB(vp), fm, tm, 0))
 		return (1);
+
+	rp->lno = fm->lno;
+	rp->cno = fm->cno;
 
 	sp->rptlines[L_YANKED] += tm->lno - fm->lno + 1;
 	return (0);
