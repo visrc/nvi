43318b4c7bf10dbdb607c9356c23404b55778d62
bostic <bostic>
1993-09-09 14:00:50 +0000

    move ex_visual into its own file

--- ex/ex_edit.c
+++ ex/ex_edit.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_edit.c,v 8.6 1993/09/09 10:41:22 bostic Exp $ (Berkeley) $Date: 1993/09/09 10:41:22 $";
+static char sccsid[] = "$Id: ex_edit.c,v 8.7 1993/09/09 14:00:50 bostic Exp $ (Berkeley) $Date: 1993/09/09 14:00:50 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -69,76 +69,5 @@ ex_edit(sp, ep, cmdp)
 	sp->n_ep = tep;
 	sp->n_frp = frp;
 	F_SET(sp, F_ISSET(cmdp, E_FORCE) ? S_FSWITCH_FORCE : S_FSWITCH);
-	return (0);
-}
-
-/*
- * ex_visual -- :[line] vi[sual] [^-.+] [window_size] [flags]
- *
- *	Switch to visual mode.
- */
-int
-ex_visual(sp, ep, cmdp)
-	SCR *sp;
-	EXF *ep;
-	EXCMDARG *cmdp;
-{
-	size_t len;
-	int pos;
-	char buf[256];
-
-	/* If a line specified, move to that line. */
-	if (cmdp->addrcnt)
-		sp->lno = cmdp->addr1.lno;
-
-	/*
-	 * Push a command based on the line position flags.  If no
-	 * flag specified, the line goes at the top of the screen.
-	 */
-	switch (F_ISSET(cmdp, E_F_CARAT | E_F_DASH | E_F_DOT | E_F_PLUS)) {
-	case E_F_CARAT:
-		pos = '^';
-		break;
-	case E_F_DASH:
-		pos = '-';
-		break;
-	case E_F_DOT:
-		pos = '.';
-		break;
-	case E_F_PLUS:
-	default:
-		pos = '+';
-		break;
-	}
-
-	if (F_ISSET(cmdp, E_COUNT))
-		len = snprintf(buf, sizeof(buf),
-		     "%luz%lu%c", sp->lno, pos, cmdp->count);
-	else
-		len = snprintf(buf, sizeof(buf), "%luz%c", sp->lno, pos);
-	(void)term_push(sp, &sp->tty, buf, len);
-
-	/*
-	 * !!!
-	 * Historically, if no line address was specified, the [p#l] flags
-	 * caused the cursor to be moved to the last line of the file, which
-	 * was then positioned as described above.  This seems useless, so
-	 * I haven't implemented it.
-	 */
-	switch (F_ISSET(cmdp, E_F_HASH | E_F_LIST | E_F_PRINT)) {
-	case E_F_HASH:
-		O_SET(sp, O_NUMBER);
-		break;
-	case E_F_LIST:
-		O_SET(sp, O_LIST);
-		break;
-	case E_F_PRINT:
-		break;
-	}
-
-	/* Switch modes. */
-	F_CLR(sp, S_MODE_EX);
-	F_SET(sp, S_MODE_VI);
-
 	return (0);
 }
