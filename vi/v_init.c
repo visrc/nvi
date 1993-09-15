6c98e22414820679129f44c6b98a58b1c27b5cd2
bostic <bostic>
1993-09-15 15:14:55 +0000

    make a machine dependent version of fwopen in PORT/clib/fwopen.c

--- vi/v_init.c
+++ vi/v_init.c
@@ -6,58 +6,18 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_init.c,v 8.8 1993/08/27 11:45:30 bostic Exp $ (Berkeley) $Date: 1993/08/27 11:45:30 $";
+static char sccsid[] = "$Id: v_init.c,v 8.9 1993/09/15 15:14:55 bostic Exp $ (Berkeley) $Date: 1993/09/15 15:14:55 $";
 #endif /* not lint */
 
 #include <sys/types.h>
 
+#include <errno.h>
 #include <stdlib.h>
 #include <string.h>
-#include <paths.h>
 
 #include "vi.h"
 #include "vcmd.h"
 #include "excmd.h"
-
-#ifdef	FWOPEN_NOT_AVAILABLE
-#include <sys/types.h>
-#include <sys/syscall.h>			/* SYS_write. */
-
-#include <unistd.h>
-
-/*
- * The fwopen call substitutes a local routine for the write system call.
- * This allows vi to trap all of the writes done via stdio by ex.  If you
- * don't have fwopen or a similar way of replacing stdio's calls to write,
- * the following kludge may work.  It depends on your loader not realizing
- * that it's being tricked and linking the C library with the real write
- * system call.  Getting the real write system call is a bit tricky, there
- * are two possible ways listed below.
- */
-int
-write(fd, buf, n)
-	int fd;
-	const void *buf;
-	size_t n;
-{
-	SCR *sp;
-
-	/*
-	 * Walk the list of screens, and see if anyone is trapping
-	 * this file descriptor.
-	 */
-	for (sp = __global_list->scrhdr.next;
-	    sp != (SCR *)&__global_list->scrhdr; sp = sp->next)
-		if (fd == sp->trapped_fd)
-			return (sp->s_ex_write(sp, buf, n));
-
-#ifdef SYS_write
-	return (syscall(SYS_write, fd, buf, n));
-#else
-	return (_write(fd, buf, n));
-#endif
-}
-#endif
 
 /*
  * v_init --
@@ -71,14 +31,10 @@ v_init(sp, ep)
 	size_t len;
 
 	/* Make ex display to a special function. */
-#ifdef FWOPEN_NOT_AVAILABLE
-	if ((sp->stdfp = fopen(_PATH_DEVNULL, "w")) == NULL)
+	if ((sp->stdfp = fwopen(sp, sp->s_ex_write)) == NULL) {
+		msgq(sp, M_ERR, "ex output: %s", strerror(errno));
 		return (1);
-	sp->trapped_fd = fileno(sp->stdfp);
-#else
-	if ((sp->stdfp = fwopen(sp, sp->s_ex_write)) == NULL)
-		return (1);
-#endif
+	}
 #ifdef MAKE_EX_OUTPUT_LINE_BUFFERED
 	(void)setvbuf(sp->stdfp, NULL, _IOLBF, 0);
 #endif
