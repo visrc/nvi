9c21b5d89ead3b56b689510947ba5d99fee30ed9
bostic <bostic>
1993-09-16 09:38:36 +0000

    clean up tag file error names

--- ex/tag.h
+++ ex/tag.h
@@ -4,13 +4,13 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: tag.h,v 8.3 1993/09/16 09:36:25 bostic Exp $ (Berkeley) $Date: 1993/09/16 09:36:25 $
+ *	$Id: tag.h,v 8.4 1993/09/16 09:38:36 bostic Exp $ (Berkeley) $Date: 1993/09/16 09:38:36 $
  */
 
 typedef struct _tagf {				/* Tag file. */
 	char	*fname;				/* Tag file name. */
 #define	TAGF_DNE	0x01			/* Didn't exist. */
-#define	TAGF_ERROR	0x02			/* Error reported. */
+#define	TAGF_DNE_WARN	0x02			/* DNE error reported. */
 	u_char	 flags;
 } TAGF;
 
