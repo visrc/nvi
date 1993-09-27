31f485722b2fc070bd08d560a540e327b0eb6007
bostic <bostic>
1993-09-27 16:20:34 +0000

    put the key in the MARK (see log.c comment for why) random prettiness policing

--- common/log.h
+++ common/log.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: log.h,v 8.1 1993/06/09 22:21:18 bostic Exp $ (Berkeley) $Date: 1993/06/09 22:21:18 $
+ *	$Id: log.h,v 8.2 1993/09/27 16:20:34 bostic Exp $ (Berkeley) $Date: 1993/09/27 16:20:34 $
  */
 
 #define	LOG_NOTYPE		0
@@ -17,11 +17,11 @@
 #define	LOG_LINE_RESET_B	7
 #define	LOG_MARK		8
 
-int	log_backward __P((struct _scr *, struct _exf *, struct _mark *));
-int	log_cursor __P((struct _scr *, struct _exf *));
-int	log_end __P((struct _scr *, struct _exf *));
-int	log_forward __P((struct _scr *, struct _exf *, struct _mark *));
-int	log_init __P((struct _scr *, struct _exf *));
-int	log_line __P((struct _scr *, struct _exf *, recno_t, u_int));
-int	log_mark __P((struct _scr *, struct _exf *, u_int, struct _mark *));
-int	log_setline __P((struct _scr *, struct _exf *, struct _mark *));
+int	log_backward __P((SCR *, EXF *, MARK *));
+int	log_cursor __P((SCR *, EXF *));
+int	log_end __P((SCR *, EXF *));
+int	log_forward __P((SCR *, EXF *, MARK *));
+int	log_init __P((SCR *, EXF *));
+int	log_line __P((SCR *, EXF *, recno_t, u_int));
+int	log_mark __P((SCR *, EXF *, MARK *));
+int	log_setline __P((SCR *, EXF *, MARK *));
