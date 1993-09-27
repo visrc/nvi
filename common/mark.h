f519181128e8d1d44f9829fa7947262b5b121687
bostic <bostic>
1993-09-27 16:21:51 +0000

    change MARK's into linked lists, getting rid of one more array put the name and some flags into the MARK to make messages and logging cleaner; undo of marks now works!

--- common/mark.h
+++ common/mark.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: mark.h,v 8.1 1993/06/09 22:21:30 bostic Exp $ (Berkeley) $Date: 1993/06/09 22:21:30 $
+ *	$Id: mark.h,v 8.2 1993/09/27 16:21:51 bostic Exp $ (Berkeley) $Date: 1993/09/27 16:21:51 $
  */
 
 /*
@@ -13,20 +13,26 @@
  * 1 based, while the column number is 0 based.  Additionally, it is known that
  * the out-of-band line number is less than any legal line number.  The line
  * number is of type recno_t, as that's the underlying type of the database.
- * The column number is of type size_t so that we can malloc a line.
+ * The column number is of type size_t, guaranteeing that we can malloc a line.
  */
 typedef struct _mark {
+	struct _mark	*next, *prev;		/* Linked list of marks. */
 #define	OOBLNO		0			/* Out-of-band line number. */
-	recno_t lno;				/* Line number. */
-	size_t cno;				/* Column number. */
+	recno_t	lno;				/* Line number. */
+	size_t	cno;				/* Column number. */
+	CHAR_T	name;				/* Mark name. */
+
+#define	MARK_DELETED	0x01			/* Mark was deleted. */
+#define	MARK_USERSET	0x02			/* User set this mark. */
+	u_char	flags;
 } MARK;
 
 #define	ABSMARK1	'\''			/* Absolute mark name. */
 #define	ABSMARK2	'`'			/* Absolute mark name. */
 
 /* Mark routines. */
-void	 mark_delete __P((struct _scr *, struct _exf *, MARK *, MARK *, int));
-MARK	*mark_get __P((struct _scr *, struct _exf *, int));
+void	 mark_delete __P((struct _scr *, struct _exf *, recno_t));
+MARK	*mark_get __P((struct _scr *, struct _exf *, ARG_CHAR_T));
 int	 mark_init __P((struct _scr *, struct _exf *));
-void	 mark_insert __P((struct _scr *, struct _exf *, MARK *, MARK *));
-int	 mark_set __P((struct _scr *, struct _exf *, int, MARK *));
+void	 mark_insert __P((struct _scr *, struct _exf *, recno_t));
+int	 mark_set __P((struct _scr *, struct _exf *, ARG_CHAR_T, MARK *, int));
