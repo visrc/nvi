3d143a675f52e46b13b3a38ad1c3c71f87311f59
bostic <bostic>
1993-09-30 14:13:42 +0000

    yank to 0.73

--- ex/ex_version.c
+++ ex/ex_version.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_version.c,v 8.9 1993/09/16 16:57:39 bostic Exp $ (Berkeley) $Date: 1993/09/16 16:57:39 $";
+static char sccsid[] = "$Id: ex_version.c,v 8.10 1993/09/30 14:13:42 bostic Exp $ (Berkeley) $Date: 1993/09/30 14:13:42 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -25,6 +25,6 @@ ex_version(sp, ep, cmdp)
 	EXCMDARG *cmdp;
 {
 	(void)fprintf(sp->stdfp,
-	    "Version 0.72 (The CSRG, U.C. Berkeley.), %s\n", "$Date: 1993/09/16 16:57:39 $");
+	    "Version 0.73 (The CSRG, U.C. Berkeley.), %s\n", "$Date: 1993/09/30 14:13:42 $");
 	return (0);
 }
