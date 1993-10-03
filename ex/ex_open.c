3eaf8f85e44ccd6627e4e2a7ea986d93a6859446
bostic <bostic>
1993-10-03 15:23:53 +0000

    the O_OPEN option turns off open

--- ex/ex_open.c
+++ ex/ex_open.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_open.c,v 8.1 1993/10/03 14:31:54 bostic Exp $ (Berkeley) $Date: 1993/10/03 14:31:54 $";
+static char sccsid[] = "$Id: ex_open.c,v 8.2 1993/10/03 15:23:53 bostic Exp $ (Berkeley) $Date: 1993/10/03 15:23:53 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -25,7 +25,13 @@ ex_open(sp, ep, cmdp)
 	EXF *ep;
 	EXCMDARG *cmdp;
 {
-	/* If open option off, disallow visual command. */
+	/* If open option off, disallow open command. */
+	if (!O_ISSET(sp, O_OPEN)) {
+		msgq(sp, M_ERR,
+		    "The open command requires that the open option be set.");
+		return (1);
+	}
+
 	msgq(sp, M_ERR, "The open command is not yet implemented.");
 	return (1);
 }
