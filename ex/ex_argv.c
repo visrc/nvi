7f830ee3ff0a08d6487aed25762e4b601a981766
bostic <bostic>
1993-10-06 17:18:20 +0000

    move ex_pipe_process here from system.o, it's only used here, now regularize the #if constructs, use #ifdef __STDC__, #if doesn't work on Suns; minimize the work, in particular, ignore signals and terminal settings

--- ex/ex_argv.c
+++ ex/ex_argv.c
@@ -6,18 +6,22 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_argv.c,v 8.10 1993/09/11 13:58:54 bostic Exp $ (Berkeley) $Date: 1993/09/11 13:58:54 $";
+static char sccsid[] = "$Id: ex_argv.c,v 8.11 1993/10/06 17:18:20 bostic Exp $ (Berkeley) $Date: 1993/10/06 17:18:20 $";
 #endif /* not lint */
 
 #include <sys/types.h>
 
 #include <ctype.h>
 #include <errno.h>
+#include <paths.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 #include "vi.h"
 #include "excmd.h"
+
+static int ex_pipe_process __P((SCR *, char *, size_t *, char *, size_t));
 
 #define	SHELLECHO	"echo "
 #define	SHELLOFFSET	(sizeof(SHELLECHO) - 1)
@@ -55,7 +59,7 @@ file_argv(sp, ep, s, argcp, argvp)
 	p = bp + SHELLOFFSET;
 	len = SHELLOFFSET;
 
-#if DEBUG && 0
+#if defined(DEBUG) && 0
 	TRACE(sp, "file_argv: {%s}\n", s);
 #endif
 
@@ -110,7 +114,7 @@ file_argv(sp, ep, s, argcp, argvp)
 	ADD_SPACE(sp, bp, blen, len + 1);
 	*p = '\0';
 
-#if DEBUG && 0
+#if defined(DEBUG) && 0
 	TRACE(sp, "pre-shell: {%s}\n", bp);
 #endif
 	/*
@@ -122,13 +126,13 @@ file_argv(sp, ep, s, argcp, argvp)
 		if (!isalnum(*p) && !isblank(*p) && *p != '/' && *p != '.')
 			break;
 	if (*p) {
-		if (ex_run_process(sp, bp, &len, bp, blen))
+		if (ex_pipe_process(sp, bp, &len, bp, blen))
 			return (1);
 		p = bp;
 	} else
 		p = bp + SHELLOFFSET;
 
-#if DEBUG && 0
+#if defined(DEBUG) && 0
 	TRACE(sp, "post-shell: {%s}\n", bp);
 #endif
 
@@ -237,7 +241,7 @@ mem1:				sp->argscnt = 0;
 	*argvp = sp->argv;
 	*argcp = off;
 
-#if DEBUG && 0
+#if defined(DEBUG) && 0
 	for (cnt = 0; cnt < off; ++cnt)
 		TRACE(sp, "arg %d: {%s}\n", cnt, sp->argv[cnt]);
 #endif
@@ -258,4 +262,103 @@ free_argv(sp)
 		FREE(sp->argv, sp->argscnt * sizeof(char *));
 	}
 	return (0);
+}
+
+/*
+ * ex_pipe_process --
+ *	Fork a process and exec a program, reading the standard out of the
+ *	program and piping it to the output of ex, whether that's really
+ *	stdout, or the vi screen.
+ */
+static int
+ex_pipe_process(sp, cmd, lenp, bp, blen)
+	SCR *sp;
+	char *cmd, *bp;
+	size_t *lenp, blen;
+{
+	FILE *ifp;
+	pid_t pid;
+	size_t len;
+	int ch, rval, output[2];
+	char *p, *sh, *sh_path, buf[1024];
+
+	sh_path = O_STR(sp, O_SHELL);
+	if ((sh = strrchr(sh_path, '/')) == NULL)
+		sh = sh_path;
+	else
+		++sh;
+
+	/*
+	 * There are two different processes running through this code.
+	 * They are named the utility and the parent. The utility reads
+	 * from standard input and writes to the parent.  The parent reads
+	 * from the utility and writes into the buffer.  The parent reads
+	 * from output[0], and the utility writes to output[1].
+	 */
+	if (pipe(output) < 0) {
+		msgq(sp, M_ERR, "Error: pipe: %s", strerror(errno));
+		return (1);
+	}
+	if ((ifp = fdopen(output[0], "r")) == NULL) {
+		msgq(sp, M_ERR, "Error: fdopen: %s", strerror(errno));
+		goto err;
+	}
+		
+	/*
+	 * Do the minimal amount of work possible, the shell is going
+	 * to run briefly and then exit.  Hopefully.
+	 */
+	switch (pid = vfork()) {
+	case -1:			/* Error. */
+		msgq(sp, M_ERR, "Error: vfork: %s", strerror(errno));
+err:		(void)close(output[0]);
+		(void)close(output[1]);
+		return (1);
+	case 0:				/* Utility. */
+		/* Redirect stdout/stderr to the write end of the pipe. */
+		(void)dup2(output[1], STDOUT_FILENO);
+		(void)dup2(output[1], STDERR_FILENO);
+
+		/* Close the utility's file descriptors. */
+		(void)close(output[0]);
+		(void)close(output[1]);
+
+		/* Assumes that all shells have -c. */
+		execl(sh_path, sh, "-c", cmd, NULL);
+		msgq(sp, M_ERR,
+		    "Error: execl: %s: %s", sh_path, strerror(errno));
+		_exit(127);
+	default:
+		/* Close the pipe end the parent won't use. */
+		(void)close(output[1]);
+		break;
+	}
+
+	rval = 0;
+
+	/* Copy process output into a buffer. */
+	for (p = bp, len = 0, ch = EOF;
+	    --blen && (ch = getc(ifp)) != EOF; *p++ = ch, ++len);
+	if (ch != EOF) {
+		rval = 1;
+		msgq(sp, M_ERR, "%s: output truncated", sh);
+	}
+	if (ferror(ifp)) {
+		rval = 1;
+		msgq(sp, M_ERR, "I/O error: %s", sh);
+	}
+	(void)fclose(ifp);
+
+	/* Wait for the process. */
+	rval |= proc_wait(sp, (long)pid, sh, 0);
+
+	/* Delete the final newline, nul terminate the string. */
+	if (p > bp && p[-1] == '\n' || p[-1] == '\r') {
+		--len;
+		*--p = '\0';
+	} else
+		*p = '\0';
+	*lenp = len;
+
+	return (rval);
 }
