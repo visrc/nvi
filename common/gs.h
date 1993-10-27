942eea2b6f45bf1d401ecc5bc12135051fd4070c
bostic <bostic>
1993-10-27 15:53:25 +0000

    add G_SIGALRM for alarm signal, too

--- common/gs.h
+++ common/gs.h
@@ -4,7 +4,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: gs.h,v 8.6 1993/10/27 15:41:45 bostic Exp $ (Berkeley) $Date: 1993/10/27 15:41:45 $
+ *	$Id: gs.h,v 8.7 1993/10/27 15:53:25 bostic Exp $ (Berkeley) $Date: 1993/10/27 15:53:25 $
  */
 
 struct _scr;
@@ -31,14 +31,15 @@ typedef struct _gs {
 
 	struct _cb	*cuts;		/* Cut buffers. */
 
-#define	G_ISFROMTTY	0x01		/* Reading from a tty. */
-#define	G_SETMODE	0x02		/* Tty mode changed. */
-#define	G_SNAPSHOT	0x04		/* Always snapshot files. */
-#define	G_TMP_INUSE	0x08		/* Temporary buffer in use. */
-#define	G_RECOVER_SET	0x10		/* Recover system initialized. */
-#define	G_SIGHUP	0x20		/* SIGHUP arrived. */
-#define	G_SIGTERM	0x40		/* SIGTERM arrived. */
-#define	G_SIGWINCH	0x80		/* SIGWINCH arrived. */
+#define	G_ISFROMTTY	0x001		/* Reading from a tty. */
+#define	G_SETMODE	0x002		/* Tty mode changed. */
+#define	G_SNAPSHOT	0x004		/* Always snapshot files. */
+#define	G_TMP_INUSE	0x008		/* Temporary buffer in use. */
+#define	G_RECOVER_SET	0x010		/* Recover system initialized. */
+#define	G_SIGALRM	0x020		/* SIGALRM arrived. */
+#define	G_SIGHUP	0x040		/* SIGHUP arrived. */
+#define	G_SIGTERM	0x080		/* SIGTERM arrived. */
+#define	G_SIGWINCH	0x100		/* SIGWINCH arrived. */
 	u_int	 flags;
 } GS;
 
