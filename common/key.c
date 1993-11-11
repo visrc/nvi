ce538aef9e69a8bded18d972b618153135dfd66a
bostic <bostic>
1993-11-11 11:16:27 +0000

    add TXT_MAPNODIGIT to get mapping right on character after last digit detect recursive macros, and recover

--- common/key.c
+++ common/key.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: key.c,v 8.17 1993/11/08 14:44:25 bostic Exp $ (Berkeley) $Date: 1993/11/08 14:44:25 $";
+static char sccsid[] = "$Id: key.c,v 8.18 1993/11/11 11:16:27 bostic Exp $ (Berkeley) $Date: 1993/11/11 11:16:27 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -120,10 +120,10 @@ term_init(sp)
  *	Push keys onto the front of a buffer.
  */
 int
-term_push(sp, ibp, push, len)
+term_push(sp, ibp, s, len)
 	SCR *sp;
 	IBUF *ibp;
-	char *push;
+	char *s;
 	size_t len;
 {
 	size_t nlen;
@@ -132,7 +132,7 @@ term_push(sp, ibp, push, len)
 	if (len <= ibp->next) {
 		ibp->next -= len;
 		ibp->cnt += len;
-		memmove(ibp->buf + ibp->next, push, len);
+		memmove(ibp->buf + ibp->next, s, len);
 		return (0);
 	}
 
@@ -148,15 +148,86 @@ term_push(sp, ibp, push, len)
 	if (ibp->cnt)
 		memmove(ibp->buf + len + TERM_PUSH_SHIFT,
 		    ibp->buf + ibp->next, ibp->cnt);
-	memmove(ibp->buf + TERM_PUSH_SHIFT, push, len);
+	if (len == 1)
+		ibp->buf[TERM_PUSH_SHIFT] = *s;
+	else
+		memmove(ibp->buf + TERM_PUSH_SHIFT, s, len);
 	ibp->next = TERM_PUSH_SHIFT;
 	ibp->cnt += len;
 	return (0);
 }
 
+/* Remove characters from a queue. */
+#define	QREM(q, len) {							\
+	if (((q)->cnt -= len) == 0)					\
+		(q)->next = 0;						\
+	else								\
+		(q)->next += len;					\
+}
+
 /*
  * term_key --
  *	Get the next key.
+ *
+ * !!!
+ * The flag TXT_MAPNODIGIT probably needs some explanation.  First, the idea
+ * of mapping keys is that one or more keystrokes behave like a function key.
+ * What's going on is that vi is reading a number, and the character following
+ * the number may or may not be mapped (TXT_MAPCOMMAND).  For example, if the
+ * user is entering the z command, a valid command is "z40+", and we don't want
+ * to map the '+', i.e. if '+' is mapped to "xxx", we don't want to change it
+ * into "z40xxx".  However, if the user is entering "35x", we want to put all
+ * of the characters through the mapping code.
+ *
+ * Historical practice is a bit muddled here.  (Surprise!)  It always permitted
+ * mapping digits as long as they weren't the first character of the map, e.g.
+ * ":map ^A1 xxx" was okay.  It also permitted the mapping of the digits 1-9
+ * (the digit 0 was a special case as it doesn't indicate the start of a count)
+ * as the first character of the map, but then ignored those mappings.  While
+ * it's probably stupid to map digits, vi isn't your mother.
+ *
+ * The way this works is that the TXT_MAPNODIGIT causes term_key to return the
+ * end-of-digit without "looking" at the next character, i.e. leaving it as the
+ * user entered it.  Presumably, the next term_key call will tell us how the
+ * user wants it handled.
+ *
+ * There is one more complication.  Users might map keys to digits, and, as
+ * it's described above, the commands "map g 1G|d2g" would return the keys
+ * "d2<end-of-digits>1G", when the user probably wanted "d21<end-of-digits>G".
+ * So, if a map starts off with a digit we continue as before, otherwise, we
+ * pretend that we haven't mapped the character and return <end-of-digits>.
+ *
+ * Now that that's out of the way, let's talk about Energizer Bunny macros.
+ * It's easy to create macros that expand to a loop, e.g. map x 3x.  It's
+ * fairly easy to detect this example, because it's all internal to term_key.
+ * If we're expanding a macro and it gets big enough, at some point we can
+ * assume it's looping and kill it.  The examples that are tough are the ones
+ * where the parser is involved, e.g. map x "ayyx"byy.  We do an expansion
+ * on 'x', and get "ayyx"byy.  We then return the first 4 characters, and then
+ * find the looping macro again.  There is no way that we can detect this
+ * without doing a full parse of the command, because the character that might
+ * cause the loop (in this case 'x') may be a literal character, e.g. the map
+ * map x "ayy"xyy"byy is perfectly legal and won't cause a loop.
+ *
+ * Historic vi tried to detect looping macros by disallowing obvious cases in
+ * the map command, maps that that ended with the same letter as they started
+ * (which wrongly disallowed map x 'x), and detecting macros that expanded too
+ * many times before keys were returned to the command parser.  It didn't get
+ * many of the tricky cases right, however, and it was certainly possible to
+ * create macros that ran forever.  Finally, changes made before vi realized
+ * that the macro was recursing were left in place.
+ *
+ * We handle the problem by returning no more than MAX_KEYS_WITHOUT_READ keys
+ * without reading from the terminal.  If we hit the boundary, we return an
+ * error and flush both terminal buffers.  This handles all of the cases, with
+ * the exception of macros that loop without returning a character.  We simply
+ * count those cases and error if there are more than MAX_MACRO_EXP expansions.
+ *
+ * XXX
+ * The final issue is recovery.  It would be possible to undo all of the work
+ * that was done by the macro if we entered a record into the log so that we
+ * knew when the macro started, and, in fact, this might be worth doing at some
+ * point.  For now we just flush the keys and leave any changes made in place.
  */
 enum input
 term_key(sp, chp, flags)
@@ -165,18 +236,23 @@ term_key(sp, chp, flags)
 	u_int flags;
 {
 	enum input rval;
+	GS *gp;
 	IBUF *keyp, *ttyp;
 	SEQ *qp;
-	int ch, ispartial, nr;
+	int ch, ispartial, ml_cnt, nr;
 
 	/* If we have expanded keys, return the next one. */
-	keyp = sp->gp->key;
-	ttyp = sp->gp->tty;
-kloop:	if (keyp->cnt) {
-		ch = keyp->buf[keyp->next++];
-		if (--keyp->cnt == 0)
-			keyp->next = 0;
-		goto beauty;
+	gp = sp->gp;
+	keyp = gp->key;
+	ttyp = gp->tty;
+loop:	if (keyp->cnt) {
+		ch = keyp->buf[keyp->next];
+		if (LF_ISSET(TXT_MAPNODIGIT) && !isdigit(ch)) {
+			*chp = NOT_DIGIT_CH;
+			return (INP_OK);
+		}
+		QREM(keyp, 1);
+		goto ret;
 	}
 
 	/*
@@ -185,6 +261,7 @@ kloop:	if (keyp->cnt) {
 	 * some number of characters.
 	 */
 	if (ttyp->cnt == 0) {
+		gp->key_cnt = 0;
 		if (rval = sp->s_key_read(sp, &nr, 0))
 			return (rval);
 		/*
@@ -200,55 +277,71 @@ kloop:	if (keyp->cnt) {
 		}
 	}
 
-	/*
-	 * Check for mapped keys. If get a partial match, copy the current
-	 * keys down in memory to maximize the space for new keys, and try
-	 * to read more keys to complete the map.
-	 */
+	/* Check for mapped keys.  */
 	if (LF_ISSET(TXT_MAPCOMMAND | TXT_MAPINPUT)) {
-mloop:		if (ttyp->buf[ttyp->next] < MAX_BIT_SEQ &&
-		    !bit_test(sp->gp->seqb, ttyp->buf[ttyp->next]))
+		ml_cnt = 0;
+newmap:		ch = ttyp->buf[ttyp->next];
+		if (ch < MAX_BIT_SEQ && !bit_test(gp->seqb, ch))
 			goto nomap;
-		qp = seq_find(sp, &ttyp->buf[ttyp->next], ttyp->cnt,
+remap:		if ((qp = seq_find(sp, &ttyp->buf[ttyp->next], ttyp->cnt,
 		    LF_ISSET(TXT_MAPCOMMAND) ? SEQ_COMMAND : SEQ_INPUT,
-		    &ispartial);
-		if (!ispartial) {
-			if (qp == NULL)
-				goto nomap;
-			ttyp->cnt -= qp->ilen;
-			if (ttyp->cnt == 0)
-				ttyp->next = 0;
-			else
-				ttyp->next += qp->ilen;
+		    &ispartial)) == NULL)
+			goto nomap;
 
-			if (O_ISSET(sp, O_REMAP)) {
-				if (term_push(sp, ttyp, qp->output, qp->olen))
-					goto err;
-				goto mloop;
-			} else
-				if (term_push(sp, keyp, qp->output, qp->olen)) {
-err:					msgq(sp, M_ERR,
-					    "Error: keys flushed: %s.",
-					    strerror(errno));
-					TERM_KEY_FLUSH(sp);
-				}
-			goto kloop;
+		/* If get a partial match, keep trying. */
+		if (ispartial) {
+			if (rval = sp->s_key_read(sp, &nr, 1))
+				return (rval);
+			if (nr)
+				goto remap;
+			goto nomap;
 		}
-		if (rval = sp->s_key_read(sp, &nr, 1))
-			return (rval);
-		if (nr)
-			goto mloop;
+
+#define	MAX_MACRO_EXP	40
+		if (++ml_cnt > MAX_MACRO_EXP)
+			goto rec_err;
+
+		if (LF_ISSET(TXT_MAPNODIGIT) && !isdigit(qp->output[0])) {
+			*chp = NOT_DIGIT_CH;
+			return (INP_OK);
+		}
+
+		QREM(ttyp, qp->ilen);
+		if (O_ISSET(sp, O_REMAP)) {
+			if (term_push(sp, ttyp, qp->output, qp->olen))
+				goto err;
+			goto newmap;
+		}
+		if (term_push(sp, keyp, qp->output, qp->olen)) {
+err:			msgq(sp, M_ERR,
+			    "Error: key deleted: %s.", strerror(errno));
+			return (INP_ERR);
+		}
+		goto loop;
 	}
 
-nomap:	ch = ttyp->buf[ttyp->next++];
-	if (--ttyp->cnt == 0)
-		ttyp->next = 0;
+nomap:	ch = ttyp->buf[ttyp->next];
+	if (LF_ISSET(TXT_MAPNODIGIT) && !isdigit(ch)) {
+		*chp = NOT_DIGIT_CH;
+		return (INP_OK);
+	}
+	QREM(ttyp, 1);
 
+#define	MAX_KEYS_WITHOUT_READ	40
+ret:	if (++gp->key_cnt > MAX_KEYS_WITHOUT_READ) {
+rec_err:	gp->key_cnt = 0;
+		TERM_FLUSH(keyp);
+		TERM_FLUSH(ttyp);
+		msgq(sp, M_ERR,
+		    "Error: command expansion too long; keys flushed.");
+		return (INP_ERR);
+	}
+		
 	/*
 	 * O_BEAUTIFY eliminates all control characters except
 	 * escape, form-feed, newline and tab.
 	 */
-beauty:	if (LF_ISSET(TXT_BEAUTIFY) && O_ISSET(sp, O_BEAUTIFY)) {
+	if (LF_ISSET(TXT_BEAUTIFY) && O_ISSET(sp, O_BEAUTIFY)) {
 		if (isprint(ch) ||
 		    sp->special[ch] == K_ESCAPE ||
 		    sp->special[ch] == K_FORMFEED ||
@@ -257,7 +350,7 @@ beauty:	if (LF_ISSET(TXT_BEAUTIFY) && O_ISSET(sp, O_BEAUTIFY)) {
 			*chp = ch;
 			return (INP_OK);
 		}
-		goto kloop;
+		goto loop;
 	}
 	*chp = ch;
 	return (INP_OK);
