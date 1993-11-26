58145bdd8b0ab8eaea48e56f45ccc8cdccae391d
bostic <bostic>
1993-11-26 15:23:09 +0000

    Historic practice: quit! or two quit's done in succession (where ZZ counts as a quit) didn't check for other files.

--- vi/v_zexit.c
+++ vi/v_zexit.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_zexit.c,v 8.3 1993/08/25 16:53:45 bostic Exp $ (Berkeley) $Date: 1993/08/25 16:53:45 $";
+static char sccsid[] = "$Id: v_zexit.c,v 8.4 1993/11/26 15:23:09 bostic Exp $ (Berkeley) $Date: 1993/11/26 15:23:09 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -31,7 +31,18 @@ v_exit(sp, ep, vp, fm, tm, rp)
 	if (F_ISSET(ep, F_MODIFIED) &&
 	    file_write(sp, ep, NULL, NULL, NULL, FS_ALL))
 		return (1);
-	if (ep->refcnt <= 1 && file_next(sp, 0) != NULL) {
+
+	/*
+	 * !!!
+	 * Historic practice: quit! or two quit's done in succession
+	 * (where ZZ counts as a quit) didn't check for other files.
+	 *
+	 * Also check for related screens; if they exist, quit, the
+	 * user will get the message on the last screen.
+	 */
+	if (sp->ccnt != sp->q_ccnt + 1 &&
+	    ep->refcnt <= 1 && file_next(sp, 0) != NULL) {
+		sp->q_ccnt = sp->ccnt;
 		msgq(sp, M_ERR,
 		    "More files to edit; use \":n\" to go to the next file");
 		return (1);
