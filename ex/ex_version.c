4558bf6f68da020386cdf03384dea63ddfe549f0
bostic <bostic>
1993-12-02 19:55:00 +0000

    ->0.89

--- ex/ex_version.c
+++ ex/ex_version.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_version.c,v 8.22 1993/11/28 18:55:03 bostic Exp $ (Berkeley) $Date: 1993/11/28 18:55:03 $";
+static char sccsid[] = "$Id: ex_version.c,v 8.23 1993/12/02 19:55:00 bostic Exp $ (Berkeley) $Date: 1993/12/02 19:55:00 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -24,10 +24,10 @@ ex_version(sp, ep, cmdp)
 	EXF *ep;
 	EXCMDARG *cmdp;
 {
-	static time_t then = 754530892;
+	static time_t then = 754880100;
 
 	(void)ex_printf(EXCOOKIE,
-"Version 0.87, %sThe CSRG, University of California, Berkeley.\n",
+"Version 0.89, %sThe CSRG, University of California, Berkeley.\n",
 	    ctime(&then));
 	return (0);
 }
