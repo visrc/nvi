40ff7741519093e03f5dfded2ae9a43715dc8d80
bostic <bostic>
1993-12-16 12:04:11 +0000

    if '$' is a motion, and deleting from at or before the first non-blank, it's a line motion delete

--- vi/v_right.c
+++ vi/v_right.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_right.c,v 8.2 1993/08/16 21:10:07 bostic Exp $ (Berkeley) $Date: 1993/08/16 21:10:07 $";
+static char sccsid[] = "$Id: v_right.c,v 8.3 1993/12/16 12:04:11 bostic Exp $ (Berkeley) $Date: 1993/12/16 12:04:11 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -64,9 +64,6 @@ v_right(sp, ep, vp, fm, tm, rp)
 /*
  * v_dollar -- [count]$
  *	Move to the last column.
- *
- *	One of places that you are allowed to move beyond the end of
- *	the line.
  */
 int
 v_dollar(sp, ep, vp, fm, tm, rp)
@@ -79,22 +76,34 @@ v_dollar(sp, ep, vp, fm, tm, rp)
 	size_t len;
 	u_long cnt;
 
-	/* A count moves down count - 1 rows, so, "3$" is the same as "2j$". */
+	/*
+	 * A count moves down count - 1 rows, so, "3$" is the same as "2j$".
+	 *
+	 * !!!
+	 * Historically, if the $ is a motion, and deleting from at or before
+	 * the first non-blank of the line, it's a line motion, and the line
+	 * motion flag is set.
+	 */
 	cnt = F_ISSET(vp, VC_C1SET) ? vp->count : 1;
 	if (cnt != 1) {
 		--vp->count;
 		if (v_down(sp, ep, vp, fm, tm, rp))
 			return (1);
-		*fm = *rp;
-	}
+		rp->cno = 0;
+		if (nonblank(sp, ep, rp->lno, &rp->cno))
+			return (1);
+		if (fm->cno <= rp->cno && F_ISSET(vp, VC_C | VC_D | VC_Y))
+			F_SET(vp, VC_LMODE);
+	} else
+		rp->lno = fm->lno;
 
-	if (file_gline(sp, ep, fm->lno, &len) == NULL) {
+	if (file_gline(sp, ep, rp->lno, &len) == NULL) {
 		if (file_lline(sp, ep, &lno))
 			return (1);
 		if (lno == 0)
 			v_eol(sp, ep, NULL);
 		else
-			GETLINE_ERR(sp, fm->lno);
+			GETLINE_ERR(sp, rp->lno);
 		return (1);
 	}
 		
@@ -103,7 +112,7 @@ v_dollar(sp, ep, vp, fm, tm, rp)
 		return (1);
 	}
 
-	rp->lno = fm->lno;
+	/* If it's a motion component, move one past the end of the line. */
 	rp->cno = F_ISSET(vp, VC_C | VC_D | VC_Y) ? len : len - 1;
 	return (0);
 }
