1b3ce095451ae5f30925515b0a5bd7f60e8e71c3
bostic <bostic>
1993-12-19 19:54:56 +0000

    Initial revision

---
+++ ex/script.h
@@ -0,0 +1,19 @@
+/*-
+ * Copyright (c) 1993
+ *	The Regents of the University of California.  All rights reserved.
+ *
+ * %sccs.include.redist.c%
+ *
+ *	$Id: script.h,v 8.1 1993/12/19 19:54:56 bostic Exp $ (Berkeley) $Date: 1993/12/19 19:54:56 $
+ */
+
+struct _script {
+	pid_t	 sh_pid;		/* Shell pid. */
+	int	 sh_master;		/* Master pty fd. */
+	int	 sh_slave;		/* Slave pty fd. */
+	char	*sh_prompt;		/* Prompt. */
+	size_t	 sh_prompt_len;		/* Prompt length. */
+	char	 sh_name[64];		/* Pty name */
+	struct winsize sh_win;		/* Window size. */
+	struct termios sh_term;		/* Terminal information. */
+};
