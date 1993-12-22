56c29de0a8335d5a45de87d081df422e31075628
bostic <bostic>
1993-12-22 13:15:47 +0000

    historic vi remapped characters in the executed @ buffer

--- ex/ex_at.c
+++ ex/ex_at.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_at.c,v 8.14 1993/12/19 22:08:51 bostic Exp $ (Berkeley) $Date: 1993/12/19 22:08:51 $";
+static char sccsid[] = "$Id: ex_at.c,v 8.15 1993/12/22 13:15:47 bostic Exp $ (Berkeley) $Date: 1993/12/22 13:15:47 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -62,8 +62,8 @@ ex_at(sp, ep, cmdp)
 	lmode = F_ISSET(cbp, CB_LMODE) || tp->q.cqe_prev != (void *)&cbp->textq;
 	for (; tp != (void *)&cbp->textq; tp = tp->q.cqe_prev)
 		if ((lmode || tp->q.cqe_prev != (void *)&cbp->textq) &&
-		    term_push(sp, "\n", 1, 0, CH_NOMAP) ||
-		    term_push(sp, tp->lb, tp->len, 0, CH_NOMAP | CH_QUOTED))
+		    term_push(sp, "\n", 1, 0, 0) ||
+		    term_push(sp, tp->lb, tp->len, 0, CH_QUOTED))
 			return (1);
 	return (0);
 }
