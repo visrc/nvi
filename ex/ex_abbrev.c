fd8a0275a9c8daee70e9c3d3375bd137da88da7d
bostic <bostic>
1994-09-02 15:33:45 +0000

    rework tests for valid abbreviatoins

--- ex/ex_abbrev.c
+++ ex/ex_abbrev.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex_abbrev.c,v 8.15 1994/08/29 09:56:31 bostic Exp $ (Berkeley) $Date: 1994/08/29 09:56:31 $";
+static char sccsid[] = "$Id: ex_abbrev.c,v 8.16 1994/09/02 15:33:45 bostic Exp $ (Berkeley) $Date: 1994/09/02 15:33:45 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -44,7 +44,7 @@ ex_abbr(sp, ep, cmdp)
 	switch (cmdp->argc) {
 	case 0:
 		if (seq_dump(sp, SEQ_ABBREV, 0) == 0)
-			msgq(sp, M_INFO, "No abbreviations to display");
+			msgq(sp, M_INFO, "233|No abbreviations to display");
 		return (0);
 	case 2:
 		break;
@@ -55,35 +55,36 @@ ex_abbr(sp, ep, cmdp)
 	/*
 	 * Check for illegal characters.
 	 *
-	 * XXX
-	 * Another fun one.  Historically, vi permitted the user to enter any
-	 * set of characters as abbreviations.  So, all of:
-	 *
-	 *	:abbr  +++i  __+++i__
-	 *	:abbr  i+++  __i+++__
-	 *	:abbr  ++++  __++++__
-	 *
-	 * were legal ex commands.  The problem arises because the text input
-	 * code which checked for an abbreviation match checked from non-word
-	 * characters to the previous whitespace character.  This means that
-	 * the first example above can be found, but the second and third can't.
+	 * !!!
+	 * Another fun one, historically.  See vi/v_ntext.c:txt_abbrev() for
+	 * details.  The bottom line is that all abbreviations have to end
+	 * with a "word" character, because it's the transition from word to
+	 * non-word characters that triggers the test for an abbreviation.  In
+	 * addition, because of the way the test is done, there can't be any
+	 * transitions from word to non-word character (or vice-versa) other
+	 * than between the next-to-last and last characters of the string,
+	 * and there can't be any <blank> characters.
 	 * Warn the user.
 	 */
-	for (p = cmdp->argv[0]->bp, len = cmdp->argv[0]->len; len; ++p, len--)
-		if (inword(*p))
-			break;
-	if (len == 0) {
+	if (!inword(cmdp->argv[0]->bp[cmdp->argv[0]->len - 1])) {
 		msgq(sp, M_ERR,
-		    "Abbreviations must contiain a least one word character");
+		    "234|Abbreviations must end with a \"word\" character");
 			return (1);
 	}
-	for (; len; ++p, len--)
-		if (!inword(*p)) {
+	for (p = cmdp->argv[0]->bp; *p != '\0'; ++p)
+		if (isblank(p[0])) {
 			msgq(sp, M_ERR,
-		    "%s may not follow word characters in an abbreviated word",
-			    KEY_NAME(sp, *p));
+			    "235|Abbreviations may not contain tabs or spaces");
 			return (1);
 		}
+	if (cmdp->argv[0]->len > 2)
+		for (p = cmdp->argv[0]->bp,
+		    len = cmdp->argv[0]->len - 2; len; --len, ++p)
+			if (inword(p[0]) != inword(p[1])) {
+				msgq(sp, M_ERR,
+"236|Abbreviations may not mix word/non-word characters, except at the end");
+				return (1);
+			}
 
 	if (seq_set(sp, NULL, 0, cmdp->argv[0]->bp, cmdp->argv[0]->len,
 	    cmdp->argv[1]->bp, cmdp->argv[1]->len, SEQ_ABBREV, SEQ_USERDEF))
@@ -104,11 +105,16 @@ ex_unabbr(sp, ep, cmdp)
         EXCMDARG *cmdp;
 {
 	ARGS *ap;
+	int nf;
+	char *p;
 
 	ap = cmdp->argv[0];
 	if (!F_ISSET(sp->gp, G_ABBREV) ||
 	    seq_delete(sp, ap->bp, ap->len, SEQ_ABBREV)) {
-		msgq(sp, M_ERR, "\"%s\" is not an abbreviation", ap->bp);
+		p = msg_print(sp, ap->bp, &nf);
+		msgq(sp, M_ERR, "237|\"%s\" is not an abbreviation", ap->bp);
+		if (nf)
+			FREE_SPACE(sp, p, 0);
 		return (1);
 	}
 	return (0);
