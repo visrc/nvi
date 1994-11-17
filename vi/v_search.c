78c2baa37e1da92358c0f72467d8e1bf097d387f
bostic <bostic>
1994-11-17 18:53:08 +0000

    make the / and ? commands be true ex addresses, matching historic practice.  Turns out that /foo/;+3 used to work...

--- vi/v_search.c
+++ vi/v_search.c
@@ -6,7 +6,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: v_search.c,v 9.1 1994/11/09 18:36:19 bostic Exp $ (Berkeley) $Date: 1994/11/09 18:36:19 $";
+static char sccsid[] = "$Id: v_search.c,v 9.2 1994/11/17 18:53:08 bostic Exp $ (Berkeley) $Date: 1994/11/17 18:53:08 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -28,22 +28,143 @@ static char sccsid[] = "$Id: v_search.c,v 9.1 1994/11/09 18:36:19 bostic Exp $ (
 
 #include "vi.h"
 #include "vcmd.h"
+#include "excmd.h"
 
-static int correct __P((SCR *, VICMDARG *, u_int));
+static int addr_search __P((SCR *, VICMDARG *, enum direction));
+static int correct __P((SCR *, VICMDARG *, int));
 static int getptrn __P((SCR *, ARG_CHAR_T, char **, size_t *));
 static int search __P((SCR *,
     VICMDARG *, char *, size_t, u_int, enum direction));
 
 /*
- * v_searchn -- n
- *	Repeat last search.
+ * v_searchb -- [count]?RE[? offset]
+ *	Search backward.
  */
 int
-v_searchn(sp, vp)
+v_searchb(sp, vp)
 	SCR *sp;
 	VICMDARG *vp;
 {
-	return (search(sp, vp, NULL, 0, SEARCH_MSG, sp->searchdir));
+	return (addr_search(sp, vp, BACKWARD));
+}
+
+/*
+ * v_searchf -- [count]/RE[/ offset]
+ *	Search forward.
+ */
+int
+v_searchf(sp, vp)
+	SCR *sp;
+	VICMDARG *vp;
+{
+	return (addr_search(sp, vp, FORWARD));
+}
+
+static int
+addr_search(sp, vp, dir)
+	SCR *sp;
+	VICMDARG *vp;
+	enum direction dir;
+{
+	MARK save;
+	size_t len;
+	char *ptrn;
+	int isdelta, nb, notused;
+
+	/*
+	 * !!!
+	 * If using the search command as a motion, any addressing components
+	 * are lost, i.e. y/ptrn/+2, when repeated, is the same as y/ptrn/.
+	 */
+	if (F_ISSET(vp, VC_ISDOT))
+		return (search(sp, vp,
+		    NULL, len, SEARCH_MSG | SEARCH_SET, dir));
+
+	/* Get a pattern.  A zero-length pattern terminates the command. */
+	if (getptrn(sp, dir == BACKWARD ? CH_BSEARCH : CH_FSEARCH, &ptrn, &len))
+		return (1);
+	if (len == 0) {
+		F_SET(vp, VM_NOMOTION);
+		return (0);
+	}
+
+	/*
+	 * !!!
+	 * Historically, vi / and ? commands were full-blown ex addresses,
+	 * including ';' delimiters, trailing <blank>'s, multiple search
+	 * strings (separated by semi-colons) and, finally, full-blown z
+	 * commands after the / and ? search strings.  (If the search was
+	 * being used as a motion, the trailing z command was ignored.)
+	 * For multiple search strings, leading <blank>'s at the second and
+	 * subsequent strings were eaten as well.  This has some unintended
+	 * side-effects: the command /ptrn/;3 is legal and results in moving
+	 * to line 3.  Should have been illegal, but it's too late now.
+	 *
+	 * !!!
+	 * Historically, if any part of the search command failed, the cursor
+	 * remained unmodified (even if ; was used).  We have to play games
+	 * because the underlying ex parser thinks we're modifying the cursor
+	 * as we go, but I think we're compatible with historic practice.
+	 *
+	 * !!!
+	 * Historically, the command "/STRING/;   " failed, apparently it
+	 * confused the parser.  We're not that compatible.
+	 *
+	 * !!!
+	 * Historically, if it wasn't a motion command, a delta in the search
+	 * pattern turns it into a first nonblank movement.
+	 */
+	save.lno = sp->lno;
+	save.cno = sp->cno;
+	for (nb = 0;;) {
+		for (; len > 0 && isblank(*ptrn); ++ptrn, --len);
+		if (len == 0)
+			break;
+
+		if (ex_line(sp, &vp->m_stop, &ptrn, &len, &notused, &isdelta))
+			goto err;
+		if (isdelta)
+			nb = 1;
+		sp->lno = vp->m_stop.lno;
+		sp->cno = vp->m_stop.cno;
+
+		for (; len > 0 && isblank(*ptrn); ++ptrn, --len);
+		if (len == 0)
+			break;
+		switch (*ptrn) {
+		case ';':	/* Ignore; the cursor is already udpated. */
+			++ptrn;
+			--len;
+			break;
+		case 'z':	/* Push the z command on the stack. */
+			if (!ISMOTION(vp) &&
+			    term_push(sp, ptrn, len, CH_NOMAP | CH_QUOTED))
+				return (1);
+			len = 0;
+			break;
+		default:
+			msgq(sp, M_ERR,
+		    "188|Characters after search string and/or line offset");
+err:			sp->lno = save.lno;
+			sp->cno = save.cno;
+			return (1);
+		}
+	}
+	sp->lno = save.lno;
+	sp->cno = save.cno;
+
+	/* Non-motion commands move to the end of the range. */
+	if (ISMOTION(vp)) {
+		if (correct(sp, vp, isdelta))
+			return (1);
+	} else {
+		vp->m_final = vp->m_stop;
+		if (nb) {
+			F_CLR(vp, VM_RCM_MASK);
+			F_SET(vp, VM_RCM_SETFNB);
+		}
+	}
+	return (0);
 }
 
 /*
@@ -64,7 +185,7 @@ v_searchN(sp, vp)
 	case FORWARD:
 		dir = BACKWARD;
 		break;
-	default:			/* NOTSET handled in search(). */
+	default:
 		dir = sp->searchdir;
 		break;
 	}
@@ -72,55 +193,15 @@ v_searchN(sp, vp)
 }
 
 /*
- * v_searchb -- [count]?RE[? offset]
- *	Search backward.
+ * v_searchn -- n
+ *	Repeat last search.
  */
 int
-v_searchb(sp, vp)
+v_searchn(sp, vp)
 	SCR *sp;
 	VICMDARG *vp;
 {
-	size_t len;
-	char *ptrn;
-
-	if (F_ISSET(vp, VC_ISDOT))
-		ptrn = NULL;
-	else {
-		if (getptrn(sp, CH_BSEARCH, &ptrn, &len))
-			return (1);
-		if (len == 0) {
-			F_SET(vp, VM_NOMOTION);
-			return (0);
-		}
-	}
-	return (search(sp, vp, ptrn, len,
-	    SEARCH_MSG | SEARCH_PARSE | SEARCH_SET, BACKWARD));
-}
-
-/*
- * v_searchf -- [count]/RE[/ offset]
- *	Search forward.
- */
-int
-v_searchf(sp, vp)
-	SCR *sp;
-	VICMDARG *vp;
-{
-	size_t len;
-	char *ptrn;
-
-	if (F_ISSET(vp, VC_ISDOT))
-		ptrn = NULL;
-	else {
-		if (getptrn(sp, CH_FSEARCH, &ptrn, &len))
-			return (1);
-		if (len == 0) {
-			F_SET(vp, VM_NOMOTION);
-			return (0);
-		}
-	}
-	return (search(sp, vp, ptrn, len,
-	    SEARCH_MSG | SEARCH_PARSE | SEARCH_SET, FORWARD));
+	return (search(sp, vp, NULL, 0, SEARCH_MSG, sp->searchdir));
 }
 
 /*
@@ -156,84 +237,33 @@ search(sp, vp, ptrn, len, flags, dir)
 	size_t len;
 	enum direction dir;
 {
-	char *eptrn;
+	char *notused;
 
 	if (ISMOTION(vp))
 		flags |= SEARCH_EOL;
 
-	for (;;) {
-		switch (dir) {
-		case BACKWARD:
-			if (b_search(sp,
-			    &vp->m_start, &vp->m_stop, ptrn, &eptrn, &flags))
-				return (1);
-			break;
-		case FORWARD:
-			if (f_search(sp,
-			    &vp->m_start, &vp->m_stop, ptrn, &eptrn, &flags))
-				return (1);
-			break;
-		case NOTSET:
-			msgq(sp, M_ERR, "187|No previous search pattern");
+	switch (dir) {
+	case BACKWARD:
+		if (b_search(sp,
+		    &vp->m_start, &vp->m_stop, ptrn, &notused, &flags))
 			return (1);
-		default:
-			abort();
-		}
-
-		/*
-		 * !!!
-		 * Historically, vi permitted trailing <blank>'s, multiple
-		 * search strings (separated by semi-colons) and full-blown
-		 * z commands after / and ? search strings.  In the case of
-		 * multiple search strings, leading <blank>'s on the second
-		 * and subsequent strings was eaten as well.
-		 *
-		 * !!!
-		 * However, the command "/STRING/;   " failed, apparently it
-		 * confused the parser.  We're not *that* compatible.
-		 *
-		 * The N, n, and ^A commands also get to here, but they've
-		 * set ptrn to NULL, len to 0, or the SEARCH_TERM flag, or
-		 * some combination thereof.
-		 */
-		if (ptrn == NULL || len == 0)
-			break;
-		len -= eptrn - ptrn;
-		for (; len > 0 && isblank(*eptrn); ++eptrn, --len);
-		if (len == 0)
-			break;
-
-		switch (*eptrn) {
-		case ';':
-			for (++eptrn; --len > 0 && isblank(*eptrn); ++eptrn);
-			ptrn = eptrn;
-			switch (*eptrn) {
-			case '/':
-				dir = FORWARD;
-				break;
-			case '?':
-				dir = BACKWARD;
-				break;
-			default:
-				goto usage;
-			}
-			ptrn = eptrn;
-			vp->m_start = vp->m_stop;
-			continue;
-		case 'z':
-			if (term_push(sp, eptrn, len, CH_NOMAP | CH_QUOTED))
-				return (1);
-			goto ret;
-		default:
-usage:			msgq(sp, M_ERR,
-		    "188|Characters after search string and/or line offset");
+		break;
+	case FORWARD:
+		if (f_search(sp,
+		    &vp->m_start, &vp->m_stop, ptrn, &notused, &flags))
 			return (1);
-		}
+		break;
+	case NOTSET:
+		msgq(sp, M_ERR, "187|No previous search pattern");
+		return (1);
+	default:
+		abort();
+		/* NOTREACHED */
 	}
 
 	/* Non-motion commands move to the end of the range. */
-ret:	if (ISMOTION(vp)) {
-		if (correct(sp, vp, flags))
+	if (ISMOTION(vp)) {
+		if (correct(sp, vp, 0))
 			return (1);
 	} else
 		vp->m_final = vp->m_stop;
@@ -282,10 +312,10 @@ getptrn(sp, prompt, ptrnp, lenp)
  * the right thing, but it's not going to exactly match historic practice.
  */
 static int
-correct(sp, vp, flags)
+correct(sp, vp, isdelta)
 	SCR *sp;
 	VICMDARG *vp;
-	u_int flags;
+	int isdelta;
 {
 	enum direction dir;
 	MARK m;
@@ -315,7 +345,7 @@ correct(sp, vp, flags)
 	 * Searches become line mode operations if there was a delta specified
 	 * to the search pattern.
 	 */
-	if (LF_ISSET(SEARCH_DELTA))
+	if (isdelta)
 		F_SET(vp, VM_LMODE);
 
 	/*
@@ -350,7 +380,7 @@ correct(sp, vp, flags)
 	 * !!!
 	 * Delta'd searches don't correct based on column positions.
 	 */
-	if (LF_ISSET(SEARCH_DELTA))
+	if (isdelta)
 		return (0);
 
 	/*
