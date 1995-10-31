42a53ca827bf3e4c1b0a404a3415a09b1952b506
bostic <bostic>
1995-10-31 11:10:09 +0000

    we now call setupterm first, regardless, so fold ti_init into it provide translation for terminfo capabilities to termcap names

--- cl/cl_bsd.c
+++ cl/cl_bsd.c
@@ -4,13 +4,15 @@
 #include <string.h>
 #include <unistd.h>
 
-static int	 first = 1;			/* Initialization flag. */
 static char	*ke;				/* Keypad on. */
 static char	*ks;				/* Keypad off. */
 static char	*vb;				/* Visible bell string. */
 
-static void	ti_init();
-
+/*
+ * HP's support the entire System V curses package except for the tigetstr
+ * and tigetnum functions.  Cthulu only knows why.
+ */
+#ifndef TIGETONLY
 /*
  * beep --
  *
@@ -42,9 +44,6 @@ bsd_putchar(ch)
 void
 flash()
 {
-	if (first)
-		ti_init();
-
 	if (vb != NULL) {
 		(void)tputs(vb, 1, bsd_putchar);
 		(void)fflush(stdout);
@@ -64,9 +63,6 @@ keypad(a, on)
 	int on;
 {
 	char *p, *sbp, sbuf[128];
-
-	if (first)
-		ti_init();
 
 	sbp = sbuf;
 	if ((p = tgetstr(on ? "ks" : "ke", &sbp)) != NULL) {
@@ -97,29 +93,85 @@ newterm(a, b, c)
  * PUBLIC: void setupterm __P((char *, int, int *));
  */
 void
-setupterm(a, b, errp)
-	char *a;
-	int b;
-	int *errp;
+setupterm(ttype, fileno, errp)
+	char *ttype;
+	int fileno, *errp;
 {
-	*errp = 1;
-	if (first)
-		ti_init();
+	static char buf[2048];
+	char *p;
+
+	if ((*errp = tgetent(buf, ttype)) > 0) {
+		if (ke != NULL)
+			free(ke);
+		ke = ((p = tigetstr("ke")) == NULL) ? NULL : strdup(p);
+		if (ks != NULL)
+			free(ks);
+		ks = ((p = tigetstr("ks")) == NULL) ? NULL : strdup(p);
+		if (vb != NULL)
+			free(vb);
+		vb = ((p = tigetstr("vb")) == NULL) ? NULL : strdup(p);
+	}
 }
+#endif /* TIGETONLY */
+
+/* Terminfo-to-termcap translation table. */
+typedef struct _tl {
+	char *terminfo;			/* Terminfo name. */
+	char *termcap;			/* Termcap name. */
+} TL;
+static const TL list[] = {
+	"cols",		"co",		/* Terminal columns. */
+	"cup",		"cm",		/* Cursor up. */
+	"cuu1",		"up",		/* Cursor up. */
+	"el",		"ce",		/* Clear to end-of-line. */
+	"kcub1",  	"kl",		/* Cursor left. */
+	"kcud1",	"kd",		/* Cursor down. */
+	"kcuf1",	"kr",		/* Cursor right. */
+	"kcuu1",  	"ku",		/* Cursor up. */
+	"kdch1",	"kD",		/* Delete character. */
+	"kdl1",		"kL",		/* Delete line. */
+	"ked",		"kS",		/* Delete to end of screen. */
+	"kel",		"kE",		/* Delete to eol. */
+	"khome",	"kh",		/* Go to sol. */
+	"kich1",	"kI",		/* Insert at cursor. */
+	"kil1",		"kA",		/* Insert line. */
+	"kind",		"kF",		/* Scroll down. */
+	"kll",		"kH",		/* Go to eol. */
+	"knp",		"kN",		/* Page down. */
+	"kpp",		"kP",		/* Page up. */
+	"kri",		"kR",		/* Scroll up. */
+	"lines",	"li",		/* Terminal lines. */
+	"rmso",		"se",		/* Standout end. */
+	"smso",		"so",		/* Standout begin. */
+};
 
 /*
- * tigetnum --
- *
- * PUBLIC: int tigetnum __P((char *));
+ * !!!
+ * Historically, the 4BSD termcap code didn't support functions keys greater
+ * than 9.  This was silently enforced -- asking for key k12 would return the
+ * value for k1.  We try and get around this by using the tables specified in
+ * the terminfo(TI_ENV) man page from the 3rd Edition SVID.  This assumes the
+ * implementors of any System V compatibility code or an extended termcap used
+ * those codes.
  */
-int
-tigetnum(name)
-	char *name;
-{
-	if (first)
-		ti_init();
+static const char codes[] = {
+/*  0-10 */ '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', ';',
+/* 11-19 */ '1', '2', '3', '4', '5', '6', '7', '8', '9',
+/* 20-63 */ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M',
+	    'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z',
+	    'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+	    'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+};
 
-	return (tgetnum(name));
+/*
+ * lcmp --
+ *	list comparison routine for bsearch.
+ */
+static int
+lcmp(a, b)
+	const void *a, *b;
+{
+	return (strcmp(a, ((TL *)b)->terminfo));
 }
 
 /*
@@ -131,50 +183,35 @@ char *
 tigetstr(name)
 	char *name;
 {
-	static char sbuf[128];
-	char *p;
+	static char sbuf[256];
+	TL *tlp;
+	int n;
+	char *p, keyname[3];
 
-	if (first)
-		ti_init();
+	if ((tlp = bsearch(name,
+	    list, sizeof(list) / sizeof(TL), sizeof(TL), lcmp)) == NULL) {
+		if (name[0] == 'k' &&
+		    name[1] == 'f' && (n = atoi(name + 2)) <= 63) {
+			keyname[0] = n <= 10 ? 'k' : 'F';
+			keyname[1] = codes[n];
+			keyname[2] = '\0';
+			name = keyname;
+		}
+	} else
+		name = tlp->termcap;
 
 	p = sbuf;
 	return (tgetstr(name, &p));
 }
 
 /*
- * ti_init --
+ * tigetnum --
+ *
+ * PUBLIC: int tigetnum __P((char *));
  */
-static void
-ti_init()
+int
+tigetnum(name)
+	char *name;
 {
-	static char buf[2048];
-	char *p;
-
-	/*
-	 * XXX
-	 * We don't want to use the options value, because we're not running
-	 * at that level.  This might fail on unusually configured systems.
-	 */
-	if ((p = getenv("TERM")) == NULL) {
-		(void)fprintf(stderr, "Environmental variable TERM not set.\n");
-		exit (1);
-	}
-
-	if (tgetent(buf, p) <= 0) {
-		(void)fprintf(stderr, "%s: tgetent failed.\n", p);
-		exit (1);
-	}
-
-	/* Reset first, so don't call back into this routine. */
-	first = 0;
-
-	if (ke != NULL)
-		free(ke);
-	ke = ((p = tigetstr("ke")) == NULL) ? NULL : strdup(p);
-	if (ks != NULL)
-		free(ks);
-	ks = ((p = tigetstr("ks")) == NULL) ? NULL : strdup(p);
-	if (vb != NULL)
-		free(vb);
-	vb = ((p = tigetstr("vb")) == NULL) ? NULL : strdup(p);
+	return (tgetnum(name));
 }
