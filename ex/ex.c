544c541249d93fdb3298947b365921ac5d1f993d
bostic <bostic>
1995-11-10 10:16:25 +0000

    major rework of information passing between ex/vi, when vi calls ex

--- ex/ex.c
+++ ex/ex.c
@@ -8,7 +8,7 @@
  */
 
 #ifndef lint
-static char sccsid[] = "$Id: ex.c,v 10.30 1995/11/06 09:56:21 bostic Exp $ (Berkeley) $Date: 1995/11/06 09:56:21 $";
+static char sccsid[] = "$Id: ex.c,v 10.31 1995/11/10 10:16:25 bostic Exp $ (Berkeley) $Date: 1995/11/10 10:16:25 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -63,14 +63,8 @@ ex(spp)
 	/* Start the ex screen. */
 	if (gp->scr_screen(sp, S_EX))
 		return (1);
-	F_CLR(sp, S_VI | S_EX_CANON | S_SCREEN_READY);
-	F_SET(sp, S_EX);
-
-	/* Initialize the rows/columns. */
 	ex_e_resize(sp);
-
-	/* Ready for messages. */
-	F_SET(sp, S_SCREEN_READY);
+	F_SET(sp, S_EX | S_SCR_EX);
 
 	/* Flush any saved messages. */
 	while ((mp = gp->msgq.lh_first) != NULL) {
