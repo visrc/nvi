d117f2f9d36e43082bb6c4b552cce01128dd588e
bostic <bostic>
1995-11-10 10:11:54 +0000

    add in_ex field to CLP so that the cl directory knows whether ex or vi exited; major rework of information passing between ex/vi, when vi calls ex

--- cl/cl.h
+++ cl/cl.h
@@ -6,7 +6,7 @@
  *
  * %sccs.include.redist.c%
  *
- *	$Id: cl.h,v 10.6 1995/11/06 19:31:49 bostic Exp $ (Berkeley) $Date: 1995/11/06 19:31:49 $
+ *	$Id: cl.h,v 10.7 1995/11/10 10:11:54 bostic Exp $ (Berkeley) $Date: 1995/11/10 10:11:54 $
  */
 
 typedef struct _cl_private {
@@ -23,6 +23,8 @@ typedef struct _cl_private {
 	char	*cuu1;		/* Cursor up terminal string. */
 	char	*rmso, *smso;	/* Inverse video terminal strings. */
 
+	int	 in_ex;		/* XXX: Currently running ex. */
+
 	int	 killersig;	/* Killer signal. */
 #define	INDX_HUP	0
 #define	INDX_INT	1
@@ -32,14 +34,12 @@ typedef struct _cl_private {
 	struct sigaction oact[INDX_MAX];
 
 #define	CL_LLINE_IV	0x0001	/* Last line is in inverse video. */
-#define	CL_SCR_EX	0x0002	/* Ex initialized and running. */
-#define	CL_SCR_EX_INIT	0x0004	/* Vi initialized. */
-#define	CL_SCR_VI	0x0008	/* Vi running. */
-#define	CL_SCR_VI_INIT	0x0010	/* Vi initialized. */
-#define	CL_SIGHUP	0x0020	/* SIGHUP arrived. */
-#define	CL_SIGINT	0x0040	/* SIGINT arrived. */
-#define	CL_SIGTERM	0x0080	/* SIGTERM arrived. */
-#define	CL_SIGWINCH	0x0100	/* SIGWINCH arrived. */
+#define	CL_SCR_EX_INIT	0x0002	/* Ex screen initialized. */
+#define	CL_SCR_VI_INIT	0x0004	/* Vi screen initialized. */
+#define	CL_SIGHUP	0x0008	/* SIGHUP arrived. */
+#define	CL_SIGINT	0x0010	/* SIGINT arrived. */
+#define	CL_SIGTERM	0x0020	/* SIGTERM arrived. */
+#define	CL_SIGWINCH	0x0040	/* SIGWINCH arrived. */
 	u_int16_t flags;
 } CL_PRIVATE;
 
@@ -54,10 +54,10 @@ typedef enum { INP_OK=0, INP_EOF, INP_ERR, INP_INTR, INP_TIMEOUT } input_t;
 
 /* Some functions can be safely ignored until the screen is running. */
 #define	VI_INIT_IGNORE(sp)						\
-	if (F_ISSET(sp, S_VI) && !F_ISSET(CLP(sp), CL_SCR_VI))		\
+	if (F_ISSET(sp, S_VI) && !F_ISSET(sp, S_SCR_VI))		\
 		return (0);
 #define	EX_INIT_IGNORE(sp)						\
-	if (F_ISSET(sp, S_EX) && !F_ISSET(CLP(sp), CL_SCR_EX))		\
+	if (F_ISSET(sp, S_EX) && !F_ISSET(sp, S_SCR_EX))		\
 		return (0);
 
 #include "cl_extern.h"
