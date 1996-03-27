05643f425d9d9ed6367240eca9209b2a48271978
bostic <bostic>
1996-03-27 08:38:31 +0000

    Move version string into VERSION file, out of ex_version.c

--- ex/ex_version.c
+++ ex/ex_version.c
@@ -10,21 +10,15 @@
 #include "config.h"
 
 #ifndef lint
-static const char sccsid[] = "$Id: ex_version.c,v 10.23 1996/03/22 11:51:50 bostic Exp $ (Berkeley) $Date: 1996/03/22 11:51:50 $";
+static const char sccsid[] = "$Id: ex_version.c,v 10.24 1996/03/27 08:38:31 bostic Exp $ (Berkeley) $Date: 1996/03/27 08:38:31 $";
 #endif /* not lint */
 
 #include <sys/types.h>
 #include <sys/queue.h>
-#ifdef TM_IN_SYS_TIME
-#include <sys/time.h>
-#endif
 
 #include <bitstring.h>
 #include <limits.h>
 #include <stdio.h>
-#ifndef TM_IN_SYS_TIME
-#include <time.h>
-#endif
 
 #include "../common/common.h"
 
@@ -39,13 +33,6 @@ ex_version(sp, cmdp)
 	SCR *sp;
 	EXCMD *cmdp;
 {
-	static const time_t then = 827513488;
-
-	struct tm *t;
-
-	t = localtime(&then);
-	(void)ex_printf(sp,
-"Version 1.59 (%02d/%02d/%d) The CSRG, University of California, Berkeley\n",
-	    t->tm_mon + 1, t->tm_mday, t->tm_year + 1900);
+	(void)ex_printf(sp, "%s\n", EX_VERSION);
 	return (0);
 }
