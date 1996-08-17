9784193c573b4bc4e252eae73780d85da37d056c
bostic <bostic>
1996-08-17 12:36:35 +0000

    convert nvi to shareware, GNU/FSF won't accept a BSDI advertisement

--- ex/ex_version.c
+++ ex/ex_version.c
@@ -10,7 +10,7 @@
 #include "config.h"
 
 #ifndef lint
-static const char sccsid[] = "$Id: ex_version.c,v 10.28 1996/07/14 12:12:17 bostic Exp $ (Berkeley) $Date: 1996/07/14 12:12:17 $";
+static const char sccsid[] = "$Id: ex_version.c,v 10.29 1996/08/17 12:36:35 bostic Exp $ (Berkeley) $Date: 1996/08/17 12:36:35 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -23,24 +23,9 @@ static const char sccsid[] = "$Id: ex_version.c,v 10.28 1996/07/14 12:12:17 bost
 #include "../common/common.h"
 #include "version.h"
 
-#define	BSDI_ADVERT \
-"\nBerkeley Software Design, Inc. (BSDI) is the commercial supplier of \
-the state-of-the-art BSD operating system, networking and Internet \
-technologies originally developed by the Computer Systems Research \
-Group (CSRG) at the University of California at Berkeley.  BSDI's \
-BSD/OS represents over twenty years of development by the worldwide \
-BSD community.  BSD technology is known for its flexible and portable \
-architecture and advanced development environments.  Today, BSDI is \
-recognized for the strength of BSDI-powered systems in demanding \
-business and technical computing environments, the worldwide customer \
-acceptance of BSD-based technology, the know-how of BSDI's leading \
-computer scientists, and BSDI's focus on delivering and supporting \
-industrial-strength software for computing platforms.  BSDI may be \
-contacted at http://www.bsdi.com/, info@bsdi.com or 1-800-800-4273."
-
 /*
  * ex_version -- :version
- *	Display the program version and a shameless plug for BSDI.
+ *	Display the program version.
  *
  * PUBLIC: int ex_version __P((SCR *, EXCMD *));
  */
@@ -49,7 +34,8 @@ ex_version(sp, cmdp)
 	SCR *sp;
 	EXCMD *cmdp;
 {
+	msgq(sp, M_INFO,
+	    "This program is shareware: see the LICENSE file for details.");
 	msgq(sp, M_INFO, VI_VERSION);
-	msgq(sp, M_INFO, BSDI_ADVERT);
 	return (0);
 }
