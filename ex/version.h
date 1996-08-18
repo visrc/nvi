e44e99d99111a22383c94ddd1d0cae742586e9a3
bostic <bostic>
1996-08-18 14:29:53 +0000

    go to 1.74

--- ex/version.h
+++ ex/version.h
@@ -1,2 +1,2 @@
 #define	VI_VERSION \
-	"Version $Revision: 1.73 $ (%H%) The CSRG, University of California, Berkeley."
+	"Version $Revision: 1.74 $ (%H%) The CSRG, University of California, Berkeley."
