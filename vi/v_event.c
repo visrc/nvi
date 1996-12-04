159afbede08a36ebeade8908a6f867f369ea97a4
bostic <bostic>
1996-12-04 19:09:06 +0000

    we can't handle E_WRESIZE here, it has to be handled in the main loop.  rename vs_repaint -> v_erepaint

--- vi/v_event.c
+++ vi/v_event.c
@@ -8,7 +8,7 @@
 #include "config.h"
 
 #ifndef lint
-static const char sccsid[] = "$Id: v_event.c,v 8.1 1996/12/04 10:03:09 bostic Exp $ (Berkeley) $Date: 1996/12/04 10:03:09 $";
+static const char sccsid[] = "$Id: v_event.c,v 8.2 1996/12/04 19:09:06 bostic Exp $ (Berkeley) $Date: 1996/12/04 19:09:06 $";
 #endif /* not lint */
 
 #include <sys/types.h>
@@ -40,6 +40,28 @@ v_equit(sp, vp)
 
 	ex_cinit(&cmd, C_QUIT, 0, OOBLNO, OOBLNO, 0, NULL);
 	return (v_exec_ex(sp, vp, &cmd));
+}
+
+/*
+ * v_erepaint --
+ *	Repaint selected lines from the screen.
+ *
+ * PUBLIC: int v_erepaint __P((SCR *, EVENT *));
+ */
+int
+v_erepaint(sp, evp)
+	SCR *sp;
+	EVENT *evp;
+{
+	SMAP *smp;
+
+	for (; evp->e_flno <= evp->e_tlno; ++evp->e_flno) {
+		smp = HMAP + evp->e_flno - 1;
+		SMAP_FLUSH(smp);
+		if (vs_line(sp, smp, NULL, NULL))
+			return (1);
+	}
+	return (0);
 }
 
 /*
@@ -88,24 +110,6 @@ v_ewriteas(sp, vp)
 }
 
 /*
- * This array maps events to vi command functions.
- */
-static VIKEYS const vievents[] = {
-/* 0	E_MOVE */
-	{v_mmark,	V_ABS_L|V_MOVE},
-/* 1	E_QUIT */
-	{v_equit},
-/* 2	E_WQ */
-	{v_ewq},
-/* 3	E_WRESIZE */
-	{NULL},
-/* 4	E_WRITE */
-	{v_ewrite},
-/* 5	E_WRITEAS */
-	{v_ewriteas},
-};
-
-/*
  * v_event --
  *	Find the event associated with a fucntion.
  */
@@ -114,6 +118,15 @@ v_event(sp, vp)
 	SCR *sp;
 	VICMD *vp;
 {
+	/* This array maps events to vi command functions. */
+	static VIKEYS const vievents[] = {
+		{v_mmark,	V_ABS_L|V_MOVE},	/* E_MOVE */
+		{v_equit,	0},			/* E_QUIT */
+		{v_ewq,		0},			/* E_WQ */
+		{v_ewrite,	0},			/* E_WRITE */
+		{v_ewriteas,	0},			/* E_WRITEAS */
+	};
+
 	switch (vp->ev.e_event) {
 	case E_MOVE:
 		vp->kp = &vievents[0];
@@ -123,9 +136,6 @@ v_event(sp, vp)
 		break;
 	case E_WQ:
 		vp->kp = &vievents[2];
-		break;
-	case E_WRESIZE:
-		vp->kp = &vievents[3];
 		break;
 	case E_WRITE:
 		vp->kp = &vievents[4];
