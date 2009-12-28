1305dd66ab76f56900796544e4e3af8b64d638af
Sven Verdoolaege <skimo@kotnet.org>
2009-12-28 20:57:49 +0100

    db: avoid use of errno field name
    If errno is defined, then using errno as a field name can give
    strange effects.

--- db.1.85/hash/ndbm.c
+++ db.1.85/hash/ndbm.c
@@ -180,7 +180,7 @@ dbm_error(db)
 	HTAB *hp;
 
 	hp = (HTAB *)db->internal;
-	return (hp->errno);
+	return (hp->_errno);
 }
 
 extern int
@@ -190,7 +190,7 @@ dbm_clearerr(db)
 	HTAB *hp;
 
 	hp = (HTAB *)db->internal;
-	hp->errno = 0;
+	hp->_errno = 0;
 	return (0);
 }
 
